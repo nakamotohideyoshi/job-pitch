37dc6c0e305921d2e351394abcc2b6558132a2ff
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2015-03-12 13:32:56 +0000

    Added dev settings

---
+++ web/src/dev.py
@@ -0,0 +1,17 @@
+import settings
+import sys
+globals().update(vars(sys.modules['settings']))
+
+DEBUG = True
+
+TEMPLATE_DEUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+

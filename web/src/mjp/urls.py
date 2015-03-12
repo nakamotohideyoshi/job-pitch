dd31a6fd2adee284294efaf8365e68d05e198486
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2015-03-12 21:17:08 +0000

    Models

--- web/src/mjp/urls.py
+++ web/src/mjp/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mjp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+import views
 
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = patterns('',
+    url(r'^api/', include(router.urls)),
+#     url(r'^api/users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )

826327d0bf18678c47ffd9e20c17f57bc7b1eeca
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2015-03-16 23:25:30 +0000

    Attempt at REST API

--- web/src/mjp/urls.py
+++ web/src/mjp/urls.py
@@ -3,14 +3,8 @@ from django.contrib import admin
 
 import views
 
-from rest_framework.routers import DefaultRouter
-
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-
 urlpatterns = patterns('',
-    url(r'^api/', include(router.urls)),
+    url(r'^api/', include(views.router.urls)),
 #     url(r'^api/users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )

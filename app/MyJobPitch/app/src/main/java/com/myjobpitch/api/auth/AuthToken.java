35fa027e926713a4d4184d19dccf0fefb4eb9542
Jamie Cockburn <jcockburn@bloxx.com>
2015-07-03 12:22:09 +0100

    Fixed upload screen rotaion issue

--- app/MyJobPitch/app/src/main/java/com/myjobpitch/api/auth/AuthToken.java
+++ app/MyJobPitch/app/src/main/java/com/myjobpitch/api/auth/AuthToken.java
@@ -2,6 +2,12 @@ package com.myjobpitch.api.auth;
 
 public class AuthToken {
 	String key;
+
+	public AuthToken() {}
+
+    public AuthToken(String key) {
+        this.key = key;
+    }
 	
 	public String getKey() {
 		return key;

ccb3c4cf38f4487a70ca2100ece0b31f281efa01
TheDiscript <reissswatson@gmail.com>
2015-08-06 15:43:49 +0000

    Job List page added & create job page added

---
+++ web/src/web/static/web/js/mjp-scripts/create-job.js
@@ -0,0 +1,68 @@
+$(function() {
+	// Run login check funtion with auto-redirect
+	checkLogin(true);
+	
+	//grab location id from url
+	var location_id = QueryString.id;
+	
+	//Populate selects
+	$.get( "/api/hours/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
+		for (var key in data) {
+			var obj = data[key];
+			$('#hours').append('<option value="'+obj.id+'">'+obj.name+'</options>');
+		}
+	})
+	.fail(function( data ) {
+		console.log( data );
+		
+	});
+	$.get( "/api/contracts/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
+		for (var key in data) {
+			var obj = data[key];
+			$('#contract').append('<option value="'+obj.id+'">'+obj.name+'</options>');
+		}
+	})
+	.fail(function( data ) {
+		console.log( data );
+	});
+	$.get( "/api/sectors/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
+		for (var key in data) {
+			var obj = data[key];
+			$('#job_sector').append('<option value="'+obj.id+'">'+obj.name+'</options>');
+		}
+	})
+	.fail(function( data ) {
+		console.log( data );
+	});
+			  
+	//Form submit code
+ 	$('#create-job').submit(function( event ) {
+		event.preventDefault();
+		var title = $('#title').val();
+		var description = $('#description').val();
+		var job_sector = $('#job_sector').val();
+		var contract = $('#contract').val();
+		var hours = $('#hours').val();
+		
+			$.post( "/api/user-jobs/", { title: title, description: description, sector: job_sector, contract: contract, hours: hours, location:location_id, status:1, csrftoken: getCookie('csrftoken') }).done(function( data ) {
+				$('#job_id').val(data.id);
+				var formData = new FormData($('#create-job')[0]);
+					  $.ajax({
+						url: '/api/user-job-images/',
+						type: 'POST',
+						data: formData,
+						async: false,
+						cache: false,
+						contentType: false,
+						processData: false,
+						success: function (data) {
+						  		
+						}
+					  });
+				
+			  })
+			  .fail(function( data ) {
+				console.log( data.responseJSON );
+			  });
+	});
+});

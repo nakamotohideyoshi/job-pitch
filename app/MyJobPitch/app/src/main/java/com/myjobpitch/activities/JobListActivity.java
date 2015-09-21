dabe56f9c31e92e0e268f084f82d6f9d1b715ec5
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2015-09-21 17:03:35 +0100

    #10 android - Jobs list display now indicated inactivity

--- app/MyJobPitch/app/src/main/java/com/myjobpitch/activities/JobListActivity.java
+++ app/MyJobPitch/app/src/main/java/com/myjobpitch/activities/JobListActivity.java
@@ -4,10 +4,10 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
-import android.util.Log;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -27,6 +27,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.myjobpitch.R;
 import com.myjobpitch.api.data.Image;
 import com.myjobpitch.api.data.Job;
+import com.myjobpitch.api.data.JobStatus;
 import com.myjobpitch.api.data.Location;
 import com.myjobpitch.tasks.CreateReadUpdateAPITaskListener;
 import com.myjobpitch.tasks.DeleteAPITaskListener;
@@ -36,8 +37,13 @@ import com.myjobpitch.tasks.recruiter.DeleteUserJobTask;
 import com.myjobpitch.tasks.recruiter.ReadUserJobsTask;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class JobListActivity extends MJPProgressActionBarActivity  {
+
+    private Integer mStatusOpenId;
 
     private Integer location_id;
     private Location mLocation;
@@ -57,14 +63,18 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
         @Override
         public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
             mode.setTitle(R.string.job);
-            Job job = (Job) list.getItemAtPosition(list.getCheckedItemPosition());
+            Job job = getSelectedJob();
             mode.setSubtitle(job.getTitle());
             return false;
         }
 
+        private Job getSelectedJob() {
+            return (Job) list.getItemAtPosition(list.getCheckedItemPosition());
+        }
+
         @Override
         public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            final Job job = (Job) list.getItemAtPosition(list.getCheckedItemPosition());
+            final Job job = getSelectedJob();
             switch (item.getItemId()) {
                 case R.id.action_edit:
                     Intent intent = new Intent(JobListActivity.this, EditJobActivity.class);
@@ -157,7 +167,14 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
             TextView titleView = (TextView) rowView.findViewById(R.id.title);
             TextView subtitleView = (TextView) rowView.findViewById(R.id.subtiltle);
             titleView.setText(job.getTitle());
-            subtitleView.setText(job.getDescription());
+            if (job.getStatus().equals(mStatusOpenId)) {
+                subtitleView.setText(job.getDescription());
+            } else {
+                rowView.setAlpha(0.4f);
+                titleView.setTypeface(null, Typeface.ITALIC);
+                subtitleView.setTypeface(null, Typeface.ITALIC);
+                subtitleView.setText(String.format("(%s) %s", getString(R.string.inactive), job.getDescription()));
+            }
             return rowView;
         }
     }
@@ -165,6 +182,9 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mStatusOpenId = getMJPApplication().get(JobStatus.class, "OPEN").getId();
+
         setContentView(R.layout.activity_job_list);
         location_id = getIntent().getIntExtra("location_id", -1);
         list = (ListView) findViewById(R.id.job_list);
@@ -173,10 +193,23 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(JobListActivity.this, JobActivity.class);
                 Job job = (Job) list.getItemAtPosition(position);
-                intent.putExtra("job_id", job.getId());
-                startActivity(intent);
+                if (job.getStatus().equals(mStatusOpenId)) {
+                    Intent intent = new Intent(JobListActivity.this, JobActivity.class);
+                    intent.putExtra("job_id", job.getId());
+                    startActivity(intent);
+                } else {
+                    new AlertDialog.Builder(JobListActivity.this)
+                            .setMessage(getString(R.string.cant_view_inactive_job))
+                            .setCancelable(false)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            })
+                            .show();
+                }
             }
         });
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
@@ -208,6 +241,9 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
 
     private void loadJobs() {
         showProgress(true);
+        final AtomicInteger completeTasks = new AtomicInteger();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
         ReadLocationTask readLocation = new ReadLocationTask(getApi(), location_id);
         readLocation.addListener(new CreateReadUpdateAPITaskListener<Location>() {
             @Override
@@ -215,34 +251,8 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
                 mLocation = result;
                 getSupportActionBar().setTitle(mLocation.getName());
                 getSupportActionBar().setSubtitle(getString(R.string.jobs));
-                ReadUserJobsTask readJobs = new ReadUserJobsTask(getApi(), location_id);
-                readJobs.addListener(new CreateReadUpdateAPITaskListener<List<Job>>() {
-                    @Override
-                    public void onSuccess(List<Job> result) {
-                        Log.d("LocationListActivity", "success");
-                        list.setAdapter(new JobListAdapter(result));
-                        showProgress(false);
-                    }
-
-                    @Override
-                    public void onError(JsonNode errors) {
-                        Toast toast = Toast.makeText(JobListActivity.this, "Error loading locations", Toast.LENGTH_LONG);
-                        toast.show();
-                        finish();
-                    }
-
-                    @Override
-                    public void onConnectionError() {
-                        Toast toast = Toast.makeText(JobListActivity.this, "Connection Error: Please check your internet connection", Toast.LENGTH_LONG);
-                        toast.show();
-                        finish();
-                    }
-
-                    @Override
-                    public void onCancelled() {
-                    }
-                });
-                readJobs.execute();
+                if (completeTasks.incrementAndGet() == 2)
+                    showProgress(false);
             }
 
             @Override
@@ -260,10 +270,38 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
             }
 
             @Override
-            public void onCancelled() {
-            }
+            public void onCancelled() {}
         });
-        readLocation.execute();
+        readLocation.executeOnExecutor(executor);
+
+        ReadUserJobsTask readJobs = new ReadUserJobsTask(getApi(), location_id);
+        readJobs.addListener(new CreateReadUpdateAPITaskListener<List<Job>>() {
+            @Override
+            public void onSuccess(List<Job> result) {
+                list.setAdapter(new JobListAdapter(result));
+                if (completeTasks.incrementAndGet() == 2)
+                    showProgress(false);
+            }
+
+            @Override
+            public void onError(JsonNode errors) {
+                Toast toast = Toast.makeText(JobListActivity.this, "Error loading locations", Toast.LENGTH_LONG);
+                toast.show();
+                finish();
+            }
+
+            @Override
+            public void onConnectionError() {
+                Toast toast = Toast.makeText(JobListActivity.this, "Connection Error: Please check your internet connection", Toast.LENGTH_LONG);
+                toast.show();
+                finish();
+            }
+
+            @Override
+            public void onCancelled() {}
+        });
+        readJobs.executeOnExecutor(executor);
+        executor.shutdown();
     }
 
     @Override
@@ -277,41 +315,35 @@ public class JobListActivity extends MJPProgressActionBarActivity  {
     }
 
     @Override
-    public void onBackPressed() {
-        Log.d("RecruiterActivity", "back");
-        super.onBackPressed();
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.job_list, menu);
         return true;
     }
 
-            @Override
-            public boolean onOptionsItemSelected(MenuItem item) {
-                Intent intent;
-                switch (item.getItemId()) {
-                    case R.id.action_add:
-                        addJob();
-                        return true;
-                    case R.id.action_add_business:
-                        intent = new Intent(this, EditBusinessActivity.class);
-                        startActivity(intent);
-                        return true;
-                    case R.id.action_add_location:
-                        intent = new Intent(this, EditLocationActivity.class);
-                        startActivity(intent);
-                        return true;
-                    case android.R.id.home:
-                        finish();
-                        if (mLocation != null) {
-                            intent = NavUtils.getParentActivityIntent(JobListActivity.this);
-                            intent.putExtra("business_id", mLocation.getBusiness());
-                            startActivity(intent);
-                        }
-                        return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                addJob();
+                return true;
+            case R.id.action_add_business:
+                intent = new Intent(this, EditBusinessActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_add_location:
+                intent = new Intent(this, EditLocationActivity.class);
+                startActivity(intent);
+                return true;
+            case android.R.id.home:
+                finish();
+                if (mLocation != null) {
+                    intent = NavUtils.getParentActivityIntent(JobListActivity.this);
+                    intent.putExtra("business_id", mLocation.getBusiness());
+                    startActivity(intent);
+                }
+                return true;
             case R.id.action_messages:
                 startActivity(new Intent(JobListActivity.this, ConversationListActivity.class));
                 return true;

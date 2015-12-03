82a6c16d6e1a7686873cdc3048124dbb94b9db52
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2015-12-03 00:44:13 +0000

    re #50 - error validation

---
+++ ios/My Job Pitch/My Job Pitch/CreateProfile.h
@@ -0,0 +1,19 @@
+//
+//  CreateProfile.h
+//  My Job Pitch
+//
+//  Created by user on 02/12/2015.
+//  Copyright © 2015 SC Labs Ltd. All rights reserved.
+//
+
+#import "ScrollingViewController.h"
+
+@interface CreateProfile : ScrollingViewController {
+    
+}
+
+- (IBAction)logout;
+- (IBAction)showJobSeeker;
+- (IBAction)showRecruiter;
+
+@end
--- ios/My Job Pitch/My Job Pitch/MJPViewController.m
+++ ios/My Job Pitch/My Job Pitch/MJPViewController.m
@@ -20,4 +20,101 @@
     return (AppDelegate *)[[UIApplication sharedApplication] delegate];
 }
 
+- (void)showProgress:(BOOL)showProgress
+{
+    if (showProgress) {
+        mainView.hidden = YES;
+        activityIndicator.hidden = NO;
+        [activityIndicator startAnimating];
+    } else {
+        mainView.hidden = NO;
+        activityIndicator.hidden = YES;
+        [activityIndicator stopAnimating];
+    }
+}
+
+- (NSArray*) getRequiredFields
+{
+    return @[];
+}
+
+- (NSDictionary*)getFieldMap
+{
+    return @{};
+}
+
+- (NSDictionary*)getErrorViewMap
+{
+    return @{};
+}
+
+- (NSMutableDictionary*)performValidation
+{
+    NSArray *requiredFields = [self getRequiredFields];
+    NSDictionary *fieldMap = [self getFieldMap];
+    NSMutableDictionary *errors = [@{} mutableCopy];
+    for (id key in [self getRequiredFields]) {
+        if ([requiredFields containsObject:key]) {
+            UITextField *field = fieldMap[key];
+            if (field) {
+                NSString *text = [field.text
+                        stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+                if ([text isEqualToString:@""]) {
+                    NSMutableArray *keyErrors;
+                    if (errors[key]) {
+                        keyErrors = [errors[key] mutableCopy];
+                    } else {
+                        keyErrors = [@[] mutableCopy];
+                    }
+                    [keyErrors addObject:@"This field is required."];
+                    errors[key] = keyErrors;
+                }
+            }
+        }
+    }
+    return errors;
+}
+
+- (BOOL)validate {
+    NSDictionary *errors = [self performValidation];
+    [self handleErrors:errors message:nil];
+    return [errors count] == 0;
+}
+
+- (void)clearErrors
+{
+    [self handleErrors:nil message:nil];
+}
+
+- (void)handleErrors:(NSDictionary*)errors message:(NSString*)message {
+    NSDictionary *errorViewMap = [self getErrorViewMap];
+    for (id key in errorViewMap) {
+        UILabel *view = errorViewMap[key];
+        [view setText:nil];
+    }
+    if (errors) {
+        for (id key in errors) {
+            UILabel *errorText = errorViewMap[key];
+            if (errorText) {
+                if ([errors objectForKey:key]) {
+                    [errorText setText:[errors[key] firstObject]];
+                }
+            } else {
+                [[[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"Error: %@", key]
+                                            message:[errors[key] firstObject]
+                                           delegate:nil
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil, nil] show];
+            }
+        }
+    }
+    if (message) {
+        [[[UIAlertView alloc] initWithTitle:@"Error"
+                                    message:message
+                                   delegate:nil
+                          cancelButtonTitle:@"OK"
+                          otherButtonTitles:nil, nil] show];
+    }
+}
+
 @end

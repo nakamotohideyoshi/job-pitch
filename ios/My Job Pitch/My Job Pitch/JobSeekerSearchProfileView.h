99327c696c219eec7f5af2cb5db1c9d1c4741944
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2016-01-28 00:33:40 +0000

    re #50 - Job search profile (except map)

--- ios/My Job Pitch/My Job Pitch/JobSeekerSearchProfileView.h
+++ ios/My Job Pitch/My Job Pitch/JobSeekerSearchProfileView.h
@@ -10,6 +10,7 @@
 #import "ErrorTextField.h"
 #import "Profile.h"
 #import <DownPicker.h>
+#import "DownPickerMultiple.h"
 
 @protocol JobSeekerSearchProfileViewDelegate
 - (void)continue;
@@ -17,19 +18,28 @@
 
 @interface JobSeekerSearchProfileView : UIView
 
+@property (weak, nonatomic) IBOutlet ErrorTextField *sectors;
+@property (nonnull) DownPickerMultiple *sectorsPicker;
 @property (weak, nonatomic) IBOutlet ErrorTextField *contract;
 @property (nonnull) DownPicker *contractPicker;
 @property (weak, nonatomic) IBOutlet ErrorTextField *hours;
 @property (nonnull) DownPicker *hoursPicker;
+@property (weak, nonatomic) IBOutlet ErrorTextField *location;
+@property (weak, nonatomic) IBOutlet ErrorTextField *radius;
+@property (nonnull) DownPicker *radiusPicker;
 
 @property (weak, nonatomic) IBOutlet UIButton *continueButton;
 
 @property (weak, nonatomic, nullable) id<JobSeekerSearchProfileViewDelegate> delegate;
 
+@property (weak, nonatomic) UINavigationController *navigationController;
+
+- (IBAction)changeLocation:(nullable id)sender;
 - (IBAction)continue:(nullable id)sender;
 - (void)load:(nonnull Profile*)profile;
 - (void)save:(nonnull Profile*)profile;
 - (void)setContracts:(nonnull NSArray*)contractObjects;
 - (void)setHoursOptions:(nonnull NSArray*)hoursObjects;
+- (void)setSectorOptions:(nonnull NSArray*)sectorObjects;
 
 @end

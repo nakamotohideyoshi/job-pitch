13223a611d8e1e422b67f918f4ee0056646ba527
unosolo <jchbarreto@gmail.com>
2016-04-13 20:13:27 -0430

    #170-AddJobButton Center image Adding Job Button Modifyng create_job javascript then could receive companyid by url.

--- web/src/web/static/web/js/mjp-scripts/create-job.js
+++ web/src/web/static/web/js/mjp-scripts/create-job.js
@@ -1,10 +1,19 @@
-function populateCompanies(){
+function populateCompanies(companyid){
 	$.get( "/api/user-businesses/", { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
 		console.log(data);
+		var selected = '';
 		for (var key in data) {
 			var obj = data[key];
-			$('#company').append('<option value="'+obj.id+'">'+obj.name+'</options>');
+
+			selected = '';
+			if(obj.id == companyid){
+				selected = 'selected=""';
+			}
+
+			$('#company').append('<option value="'+obj.id+'" '+selected+'>'+obj.name+'</options>');
 		}
+
+		$('#company').trigger('change');
 	});
 }
 
@@ -12,16 +21,16 @@ function populateCompanies(){
 $(function() {
 	// Run login check funtion with auto-redirect
 	checkLogin(true);
-	
+
+	//grab company id from url
+	var company_id = QueryString.companyid;
+
 	//grab location id from url
 	var location_id = QueryString.id;
-	
+
 	//variables defined
 	var open_job_status;
-	
-	//Populate Companies and Locations selects
-	populateCompanies();
-	
+
 	$( "#company" ).change(function() {
 		if($(this).val() != ''){
 		   $.get( "/api/user-locations/", { business:$(this).val() ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
@@ -33,59 +42,59 @@ $(function() {
 			   })
 			.fail(function( data ) {
 				console.log( data );
-				
 			});
 		}else{
 			$('#locationSelect').hide();
 			$( "#location" ).val('');
 		}
 	});
-	
+
 	$( "#location" ).change(function() {
 		if($(this).val() != ''){
-			$('#mainFormAreaCreateJob').show();	
+			$('#mainFormAreaCreateJob').show();
 			location_id = $(this).val();
 			$.get( "/api/user-locations/"+location_id, { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
 				$('#currentLogo').attr('src', data.images[0].thumbnail).show();
-			});	
+			});
 		}else{
 			$('#locationSelect').hide();
-			$('#mainFormAreaCreateJob').hide();	
+			$('#mainFormAreaCreateJob').hide();
 			$( "#location" ).val('');
 		}
 	});
-	
-	
-	
+
+	//Populate Companies and Locations selects
+	populateCompanies(company_id);
+
 	//if location id is set then go ahead with populating the fields and do other functions
 	if(typeof location_id !== "undefined"){
 
 		$.get( "/api/user-locations/"+location_id, { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
 			$('#currentLogo').attr('src', data.images[0].thumbnail).show();
-		});	
-		
+		});
+
 		$.get( "/api/user-locations/"+location_id, { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
-			
+
 			$('#company').val(data.business_data.id);
-			
-				$.get( "/api/user-locations/", { business:$('#company').val() ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
-				   for (var key in data) {
-						var obj = data[key];
-						$('#location').append('<option value="'+obj.id+'">'+obj.name+'</options>');
-					}
-					$('#location').val(location_id);
-				   $('#locationSelect').show();
-				   
-				   })
-				.fail(function( data ) {
-					console.log( data );
-					
-				});
-			$('#mainFormAreaCreateJob').show();	
-		});	
-	
+
+			$.get( "/api/user-locations/", { business:$('#company').val() ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
+				for (var key in data) {
+					var obj = data[key];
+					$('#location').append('<option value="'+obj.id+'">'+obj.name+'</options>');
+				}
+				$('#location').val(location_id);
+			   $('#locationSelect').show();
+
+				})
+			.fail(function( data ) {
+				console.log( data );
+
+			});
+			$('#mainFormAreaCreateJob').show();
+		});
+
 	}
-	
+
 	//Populate selects
 	$.get( "/api/hours/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
 		for (var key in data) {
@@ -95,7 +104,7 @@ $(function() {
 	})
 	.fail(function( data ) {
 		console.log( data );
-		
+
 	});
 	$.get( "/api/contracts/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
 		for (var key in data) {
@@ -115,9 +124,9 @@ $(function() {
 	.fail(function( data ) {
 		console.log( data );
 	});
-	
+
 	//Get other data
-	
+
 	$.get( "/api/job-statuses/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
 		for (var key in data) {
 			var obj = data[key];
@@ -129,9 +138,9 @@ $(function() {
 	.fail(function( data ) {
 		console.log( data );
 	});
-			  
+
 	//Form submit code
- 	$('#create-job').submit(function( event ) {
+	$('#create-job').submit(function( event ) {
 		$('.btn-primary').attr( "disabled", true );
 		event.preventDefault();
 		var title = $('#title').val();
@@ -139,12 +148,12 @@ $(function() {
 		var job_sector = $('#job_sector').val();
 		var contract = $('#contract').val();
 		var hours = $('#hours').val();
-		
+
 			$.post( "/api/user-jobs/", { title: title, description: description, sector: job_sector, contract: contract, hours: hours, location:location_id, status:open_job_status, csrftoken: getCookie('csrftoken') }).done(function( data ) {
 				$('#job_id').val(data.id);
 				if($('#job_image').val() != ''){
-					  var formData = new FormData($('#create-job')[0]);
-					  $.ajax({
+					var formData = new FormData($('#create-job')[0]);
+					$.ajax({
 						url: '/api/user-job-images/',
 						type: 'POST',
 						data: formData,
@@ -153,46 +162,47 @@ $(function() {
 						contentType: false,
 						processData: false,
 						success: function (data) {
-								window.location.href = "/profile/list-jobs/?id="+location_id;
+							window.location.href = "/profile/list-jobs/?id="+location_id;
 						}
-					  });
+				  });
 				}else{
 					//window.location.href = "/profile/list-jobs/?id="+location_id;
 					$.get( "/api/user-locations/"+location_id, { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
-							console.log( data );
-							$('#currentLogo').attr('src', data.images[0].thumbnail).show();
-												  var xhr = new XMLHttpRequest();
-													xhr.onreadystatechange = function(){
-														if (this.readyState == 4 && this.status == 200){
-															//this.response is what you're looking for
-															//console.log(this.response, typeof this.response);
-															//console.log(data.images[0].image.split('.').pop());
-															$('#job_image').remove();
-															var formData = new FormData($('#create-job')[0]);
-															formData.append('image',this.response, 'location-'+location_id+'.'+data.images[0].image.split('.').pop());
-															  $.ajax({
-																url: '/api/user-job-images/',
-																type: 'POST',
-																data: formData,
-																async: false,
-																cache: false,
-																contentType: false,
-																processData: false,
-																success: function (data) {
-																		console.log(data);
-																		window.location.href = "/profile/list-jobs/?id="+location_id;
-																}
-															  });
-														}
-													}
-													xhr.open('GET', data.images[0].image);
-													xhr.responseType = 'blob';
-													xhr.send();    
+						console.log( data );
+						$('#currentLogo').attr('src', data.images[0].thumbnail).show();
+						var xhr = new XMLHttpRequest();
+
+						xhr.onreadystatechange = function(){
+							if (this.readyState == 4 && this.status == 200){
+								//this.response is what you're looking for
+								//console.log(this.response, typeof this.response);
+								//console.log(data.images[0].image.split('.').pop());
+								$('#job_image').remove();
+								var formData = new FormData($('#create-job')[0]);
+								formData.append('image',this.response, 'location-'+location_id+'.'+data.images[0].image.split('.').pop());
+								  $.ajax({
+										url: '/api/user-job-images/',
+										type: 'POST',
+										data: formData,
+										async: false,
+										cache: false,
+										contentType: false,
+										processData: false,
+										success: function (data) {
+											console.log(data);
+											window.location.href = "/profile/list-jobs/?id="+location_id;
+										}
+								  });
+							}
+						}
+						xhr.open('GET', data.images[0].image);
+						xhr.responseType = 'blob';
+						xhr.send();
 					});
 				}
-				
-			  })
-			  .fail(function( data ) {
+
+				})
+				.fail(function( data ) {
 				for (var key in data.responseJSON) {
 						var obj = data.responseJSON[key];
 						if(key == 'non_field_errors'){
@@ -206,14 +216,15 @@ $(function() {
 					$('.btn-primary').attr( "disabled", false );
 			  });
 	});
-	
+
 	var text_max = 500;
-    $('#textarea_feedback').html(text_max + ' characters remaining');
 
-    $('#description').keyup(function() {
-        var text_length = $('#description').val().length;
-        var text_remaining = text_max - text_length;
+	$('#textarea_feedback').html(text_max + ' characters remaining');
 
-        $('#textarea_feedback').html(text_remaining + ' characters remaining');
-    });
+	$('#description').keyup(function() {
+		var text_length = $('#description').val().length;
+		var text_remaining = text_max - text_length;
+
+		$('#textarea_feedback').html(text_remaining + ' characters remaining');
+	});
 });

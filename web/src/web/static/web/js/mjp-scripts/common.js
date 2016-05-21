71cdc94d90f085b6757b5c0f8bb618b8e8d29f70
unosolo <jchbarreto@gmail.com>
2016-05-21 18:27:32 -0400

    * Spacing for readability. * Call logoutUser function when authentication fails so any httponly cookies be reset.

--- web/src/web/static/web/js/mjp-scripts/common.js
+++ web/src/web/static/web/js/mjp-scripts/common.js
@@ -1,21 +1,25 @@
 /* Common Functions */
 
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/;";
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/;";
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(name) == 0){
+			return c.substring(name.length,c.length);
+		}
+	}
+
+	return "";
 }
 
 function deleteCookie(cname){
@@ -24,7 +28,6 @@ function deleteCookie(cname){
 
 //get lat long and other data from postcode
 function postcodeLocationData(postcode, handleData){
-	console.log(postcode);
 	if(postcode != ''){
 		$.ajax({ cache: false,
 			url: "http://api.postcodes.io/postcodes/"+postcode,
@@ -39,7 +42,6 @@ function postcodeLocationData(postcode, handleData){
 			}
 		});
 	}else{
-		console.log('hit');
 		fieldError('Please enter a postcode.', 'postcode');
 		$('.btn-primary').attr( "disabled", false );
 	}
@@ -51,6 +53,7 @@ function checkLogin(redirect){
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		window.location.href = "/mobile-app";
 	}
+
 	var username = getCookie('username');
 	if(username == ""){
 		if(redirect == true){
@@ -60,6 +63,7 @@ function checkLogin(redirect){
 			$('.not_logged_in_menu').show();
 			$('.not_logged_in_element').show();
 			$('.login-username').hide();
+
 			return false;
 		}
 	}else{
@@ -69,131 +73,145 @@ function checkLogin(redirect){
 	}
 }
 
+function logoutUser(){
+	deleteCookie('username');
+	deleteCookie('key');
+
+	$.post( "/api-rest-auth/logout/", {
+		csrfmiddlewaretoken: getCookie('csrftoken')
+	})
+	.done(function( data ) {
+		window.location.href = "/";
+	})
+	.fail(function( data ) {
+		window.location.href = "/";
+
+	});
+
+}
+
 function setHeaderUsername(){
 	var username = getCookie('username');
 	$('#header_username').html(username);
+
 	// Display said menu
 	$('.logged_in_menu').show();
-
-
 }
 
 function applyForJob(job_id, job_seeker_id){
-			  $.post( "/api/applications/", { job: job_id, job_seeker: job_seeker_id, csrftoken: getCookie('csrftoken')}).done(function( data ) {
-				  $('#applyButton').fadeOut(200,function(){
-						$('#job_applied_for').show();
-					});
-			  })
-			  .fail(function( data ) {
+	$.post( "/api/applications/", {
+		job: job_id,
+		job_seeker: job_seeker_id,
+		csrftoken: getCookie('csrftoken')
+	}).done(function( data ) {
+		$('#applyButton').fadeOut(200,function(){
+			$('#job_applied_for').show();
+		});
+	})
+	.fail(function( data ) {
 
-			  });
+	});
 }
 
 function connectWithJob(job_id, job_seeker_id){
-			  $.post( "/api/applications/", { job: job_id, job_seeker: job_seeker_id, csrftoken: getCookie('csrftoken')}).done(function( data ) {
-				  //location.reload();
-				  $('#viewPitchModal').find('.modal-body').html('<div class="row"><div class="col-md-12"><h4 style="text-align: center; font-size:16px;">Thanks for requesting to connect. A message has been sent to the job seeker</h4></div></div><div class="row"><div class="col-md-12" style="text-align:center;"><button style="margin-left:0;" type="button" class="btn btn-custom" data-dismiss="modal" aria-label="Close">Back to List</button></div></row>');
-				  $('#job-list-'+job_seeker_id).remove();
-			  })
-			  .fail(function( data ) {
+	$.post( "/api/applications/", {
+		job: job_id,
+		job_seeker: job_seeker_id,
+		csrftoken: getCookie('csrftoken')
+	})
+	.done(function( data ) {
+		//location.reload();
+		$('#viewPitchModal')
+		.find('.modal-body')
+		.html('<div class="row"><div class="col-md-12"><h4 style="text-align: center; font-size:16px;">Thanks for requesting to connect. A message has been sent to the job seeker</h4></div></div><div class="row"><div class="col-md-12" style="text-align:center;"><button style="margin-left:0;" type="button" class="btn btn-custom" data-dismiss="modal" aria-label="Close">Back to List</button></div></row>');
+		$('#job-list-'+job_seeker_id).remove();
+	})
+	.fail(function( data ) {
 
-			  });
+	});
 }
 
 
 function messageRead(message_id){
-			$.put( "/api/messages/"+message_id+'/', { read:true, csrftoken: getCookie('csrftoken')}).done(function( data ) {
-;
-
-			  })
-			  .fail(function( data ) {
-
-			  });
+	$.put( "/api/messages/"+message_id+'/', {
+		read:true,
+		csrftoken: getCookie('csrftoken')
+	});
 }
 
 var QueryString = function () {
-  // This function is anonymous, is executed immediately and
-  // the return value is assigned to QueryString!
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-        // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-        // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-        // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  }
-    return query_string;
+	// This function is anonymous, is executed immediately and
+	// the return value is assigned to QueryString!
+	var query_string = {};
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		// If first entry with this name
+		if (typeof query_string[pair[0]] === "undefined") {
+			query_string[pair[0]] = decodeURIComponent(pair[1]);
+
+		// If second entry with this name
+		} else if (typeof query_string[pair[0]] === "string") {
+			var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+			query_string[pair[0]] = arr;
+
+		// If third or later entry with this name
+		} else {
+			query_string[pair[0]].push(decodeURIComponent(pair[1]));
+		}
+	}
+		return query_string;
 }();
 
 // Get the user type(business or job_seeker) & sort the menus out.
 function userTypeMenuConfiguration(redirectToProfile){
-			  $.get( "/api-rest-auth/user/", { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
+	$.get( "/api-rest-auth/user/", {
+		token: getCookie('key'),
+		csrftoken: getCookie('csrftoken')
 
-				if (data.businesses.length){
-    				// business
-					$('.business-link').show();
-				}else if (data.job_seeker != null){
-	    			// job-seeker
-					$('.job-seeker-link').show();
-				}else{
-				    // Go Finish registration
-					if(redirectToProfile == true){
-						window.location.href = "/profile";
-					}
-				}
+	})
+	.done(function( data ) {
+		if (data.businesses.length){
+			// business
+			$('.business-link').show();
+		}else if (data.job_seeker != null){
+			// job-seeker
+			$('.job-seeker-link').show();
+		}else{
+			// Go Finish registration
+			if(redirectToProfile == true){
+				window.location.href = "/profile";
+			}
+		}
+	})
+	.fail(function( data ) {
+		logoutUser();
+		console.log( data );
+	});
 
-			  })
-			  .fail(function( data ) {
-				console.log( data );
-			  });
-
-}
-
-function logoutUser(){
-	var login = checkLogin();
-	if(login == true){
-			deleteCookie('username');
-			deleteCookie('key');
-
-			$.post( "/api-rest-auth/logout/", { csrfmiddlewaretoken: getCookie('csrftoken') }).done(function( data ) {
-				window.location.href = "/";
-			  })
-			  .fail(function( data ) {
-				console.log( data.responseJSON );
-			  });
-	}else{
-		window.location.href = "/login";
-	}
 }
 
 function formAlert(type, message){
-    $('.alert').addClass('alert-'+type);
-    $('.alert').html(message);
-    $('.alert').show();
+	$('.alert').addClass('alert-'+type);
+	$('.alert').html(message);
+	$('.alert').show();
 }
 
 function putManyAlerts(parentId, messages ){
-    $(parentId+' > .alert').addClass('alert-'+messages[0].type);
-    $(parentId+' > .alert').html(messages[0].content);
-    $(parentId+' > .alert').show();
+	$(parentId+' > .alert').addClass('alert-'+messages[0].type);
+	$(parentId+' > .alert').html(messages[0].content);
+	$(parentId+' > .alert').show();
 
-    for (var i = 1;  i < messages.length ; i++) {
-        message = messages[i];
-        $(parentId+' > .alert:last')
-        .clone()
-        .addClass('alert-'+message.type)
-        .html(message.content)
-        .insertAfter(parentId+' > .alert:last');
-    }
+	for (var i = 1;  i < messages.length ; i++) {
+		message = messages[i];
+
+		$(parentId+' > .alert:last')
+		.clone()
+		.addClass('alert-'+message.type)
+		.html(message.content)
+		.insertAfter(parentId+' > .alert:last');
+	}
 }
 
 function fieldError(error,field){
@@ -217,7 +235,7 @@ function deleteRow(id, apiFunction, rowPrefix){
 				console.log(rowPrefix+id);
 				$('#'+rowPrefix+id).fadeOut(250);
 			}
-			});
+		});
 	});
 }
 
@@ -231,72 +249,79 @@ function deleteRowApplication(id, rowPrefix){
 				console.log(rowPrefix+id);
 				$('#'+rowPrefix+id).fadeOut(250);
 			}
-			});
+		});
 	});
 }
 
 //check if a job seeker account is active. Returns true|false
 
 function account_active_check(){
-		$.get( "/api-rest-auth/user/", { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
+	$.get( "/api-rest-auth/user/", {
+		token: getCookie('key'),
+		csrftoken: getCookie('csrftoken')
+	})
+	.done(function( data ) {
+		job_seeker_id = data.job_seeker;
 
-				  job_seeker_id = data.job_seeker;
-
-				  $.get( "/api/job-seekers/"+data.job_seeker, { token: getCookie('key') ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
-					  return data.active;
-				  });
-
+		$.get( "/api/job-seekers/"+data.job_seeker, {
+			token: getCookie('key'),
+			csrftoken: getCookie('csrftoken')
+		}).done(function( data ) {
+			return data.active;
 		});
+
+	});
 }
 
 // Some handy helpers
 
 $.put = function(url, data, callback, type){
 
-  if ( $.isFunction(data) ){
-    type = type || callback,
-    callback = data,
-    data = {}
-  }
+	if ( $.isFunction(data) ){
+		type = type || callback,
+		callback = data,
+		data = {}
+	}
 
-  return $.ajax({
-    url: url,
-    type: 'PUT',
-    success: callback,
-    data: data,
-    contentType: type
-  });
+	return $.ajax({
+		url: url,
+		type: 'PUT',
+		success: callback,
+		data: data,
+		contentType: type
+	});
 }
 
 $.delete = function(url, data, callback, type){
 
-  if ( $.isFunction(data) ){
-    type = type || callback,
-        callback = data,
-        data = {}
-  }
+	if ( $.isFunction(data) ){
+		type = type || callback,
+		callback = data,
+		data = {}
+	}
 
-  return $.ajax({
-    url: url,
-    type: 'DELETE',
-    success: callback,
-    data: data,
-    contentType: type
-  });
+	return $.ajax({
+		url: url,
+		type: 'DELETE',
+		success: callback,
+		data: data,
+		contentType: type
+	});
 }
 
 //Fix the CSRF on the above functions
 
 function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	// these HTTP methods do not require CSRF protection
+	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
+
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    }
+	beforeSend: function(xhr, settings) {
+		if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+			xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+		}
+	}
 });
 
 function deleteJobSeekerFromJob(job_id,job_seeker){
@@ -326,6 +351,7 @@ $(function() {
 	var login = checkLogin();
 	var pathArray = window.location.pathname.split( '/' );
 	var segment_1 = pathArray[1];
+
 	if(login){
 		//redirect exeptions
 		if(segment_1 == 'profile'){
@@ -334,25 +360,35 @@ $(function() {
 			userTypeMenuConfiguration(true);
 		}
 	}
+
 	//Form submit code - Login
- 	$('#login').submit(function( event ) {
+	$('#login').submit(function( event ) {
 		event.preventDefault();
 		var username = $('#username').val();
 		var password1 = $('#password').val();
 		var csrfmiddlewaretoken = $('[name="csrfmiddlewaretoken"]').val();
-			$.post( "/api-rest-auth/login/", { username: username, password: password1, csrfmiddlewaretoken: csrfmiddlewaretoken }).done(function( data ) {
-				setCookie('username', username, 28);
-				setCookie('key', data.key, 28);
-				window.location.href = "/applications/";
-			  })
-			  .fail(function( data ) {
-				var messageError = ''
-				for (var key in data.responseJSON) {
-					var obj = data.responseJSON[key];
-					messageError = messageError+obj+'<br>';
-				}
-				formAlert('danger', messageError);
-			  });
+		$.post( "/api-rest-auth/login/", {
+			username: username,
+			password: password1,
+			csrfmiddlewaretoken: csrfmiddlewaretoken
+
+		})
+		.done(function( data ) {
+			setCookie('username', username, 28);
+			setCookie('key', data.key, 28);
+			window.location.href = "/applications/";
+
+		})
+		.fail(function( data ) {
+			var messageError = '';
+
+			for (var key in data.responseJSON) {
+				var obj = data.responseJSON[key];
+				messageError = messageError+obj+'<br>';
+			}
+
+			formAlert('danger', messageError);
+		});
 	});
 
 	//Form submit code - Reg
@@ -362,34 +398,50 @@ $(function() {
 		var password1 = $('#password1').val();
 		var password2 = $('#password2').val();
 		var csrfmiddlewaretoken = $('[name="csrfmiddlewaretoken"]').val();
-		$.post( "/api-rest-auth/registration/", { username: username, password1: password1, password2: password2, csrfmiddlewaretoken: csrfmiddlewaretoken }).done(function( data ) {
-			$.post( "/api-rest-auth/login/", { username: username, password: password1, csrfmiddlewaretoken: csrfmiddlewaretoken }).done(function( data ) {
+
+		$.post( "/api-rest-auth/registration/", {
+			username: username,
+			password1: password1,
+			password2: password2,
+			csrfmiddlewaretoken: csrfmiddlewaretoken
+		})
+		.done(function( data ) {
+			$.post( "/api-rest-auth/login/", {
+				username: username,
+				password: password1,
+				csrfmiddlewaretoken: csrfmiddlewaretoken
+			})
+			.done(function( data ) {
 				setCookie('username', username, 28);
 				setCookie('key', data.key, 28);
 				window.location.href = "/profile";
-			  })
-			  .fail(function( data ) {
+			})
+			.fail(function( data ) {
 
-			  });
-		  })
-		  .fail(function( data ) {
-
+			});
+		})
+		.fail(function( data ) {
 			var messageError = ''
+
 			for (var key in data.responseJSON) {
 				var obj = data.responseJSON[key];
 				messageError = messageError+obj+'<br>';
 			}
+
 			formAlert('danger', messageError);
-		  });
+		});
 	});
+
 	$('#regModal').on('hidden.bs.modal', function () {
 		$('.alert').html('');
 		$('.alert').hide();
 	});
+
 	$('#loginModal').on('hidden.bs.modal', function () {
 		$('.alert').html('');
 		$('.alert').hide();
 	});
+
 	$('#viewPitchModal').on('hidden.bs.modal', function () {
 		$('#viewPitchModal').find('.modal-body').html('<div class="col-md-12" id="pitchViewer"></div><div class="col-md-offset-4 col-md-4"><a class="btn btn-custom" id="applyButtonModal" style="display:none; margin-left: 18px;margin-top: 20px;">Connect</a></div>');
 	});

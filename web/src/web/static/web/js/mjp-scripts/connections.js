5fe9c7e693f862b2e35ddaf5facfd4dc60b83418
unosolo <jchbarreto@gmail.com>
2016-05-24 18:26:23 -0400

    Refactoring.

--- web/src/web/static/web/js/mjp-scripts/connections.js
+++ web/src/web/static/web/js/mjp-scripts/connections.js
@@ -9,37 +9,30 @@ $(function() {
 	// Run login check funtion with auto-redirect
 	checkLogin(true);
 
-	var job_id = QueryString.job;
-
-	var job_refine = '';
-
-	if(job_id === undefined){
-	}else{
-		job_refine = '?job='+job_id;
-	}
-
-	var businessUser = 0;
-
-	$.get( "/api-rest-auth/user/", {
-		token: getCookie('key') ,
+	var query = {
+		token: getCookie('key'),
 		csrftoken: getCookie('csrftoken')
-	}).done(function( data ) {
-		if (data.businesses.length){
-			// business
+	};
+
+	$.get( "/api-rest-auth/user/", query)
+	.done(function( user ) {
+		var businessUser = 0;
+		if (user.businesses.length){ // business
 			businessUser = 1;
 		}
 
-		$.get( "/api/application-statuses/", {
-			csrftoken: getCookie('csrftoken')
-		})
+		$.get( "/api/application-statuses/", query)
 		.done(function(statuses) {
 			var index = _.findIndex(statuses, ['name', 'ESTABLISHED']);
 			var status = statuses[index];
 
-			$.get( "/api/applications/"+job_refine, {
-				csrftoken: getCookie('csrftoken'),
-				status: status.id
-			}).done(function( applications ) {
+			var job_id = QueryString.job;
+			if(job_id !== undefined && job_id !== ''){
+				query.job = job_id // Recruiter looking for applications of a job
+			}
+
+			$.get( "/api/applications/", query)
+			.done(function( applications ) {
 				var fiteredApplications = _.filter(applications, {'status': status.id});
 
 				if(fiteredApplications.length != 0){
@@ -52,20 +45,16 @@ $(function() {
 						var date = new Date(latest_message.created);
 						var minutesTwoDigitsWithLeadingZero = ("0" + date.getMinutes()).substr(-2);
 
-						if(businessUser == 1){
-							//business
-							//if deleted
-							if(obj.status == 3){
+						if(businessUser == 1){ //business
+							if(obj.status == 3){ //if deleted
 								$('#list-table tbody')
 								.append('<tr data-application-id="'+obj.id+'" class="application-list inactive" id="application-list-'+obj.id+'"><td>'+obj.job_data.title+'</td><td>'+obj.job_seeker.first_name+' '+obj.job_seeker.last_name+'</td><td>'+obj.job_seeker.description+'</td><td>'+messageShort+'...<br><span class="message-time-date">'+date.getHours()+':'+minutesTwoDigitsWithLeadingZero+' '+date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()+'</span></td><td>Deleted</td></tr>');
 							}else{
 								$('#list-table tbody')
 								.append('<tr data-application-id="'+obj.id+'" class="application-list" id="application-list-'+obj.id+'"><td>'+obj.job_data.title+'</td><td onclick="viewPitch2(\''+obj.job_seeker.pitches[0].video+'\','+obj.job_data.id+','+obj.job_seeker.id+');">'+obj.job_seeker.first_name+' '+obj.job_seeker.last_name+'</td><td>'+obj.job_seeker.description+'</td><td onclick="goToMessages('+obj.id+');">'+messageShort+'...<br><span class="message-time-date">'+date.getHours()+':'+minutesTwoDigitsWithLeadingZero+' '+date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()+'</span></td><td><a class="delete-action-btn btn btn-primary" href="javascript:deleteRowApplication('+obj.id+' , \'application-list-\');"><i class="fa fa-trash-o"></i></a></td></tr>');
 							}
-						}else{
-							//job seeker
-							//if deleted
-							if(obj.status == 3){
+						}else{ //job seeker
+							if(obj.status == 3){ //if deleted
 								$('#list-table tbody')
 								.append('<tr data-application-id="'+obj.id+'" class="application-list inactive" id="application-list-'+obj.id+'"><td>'+obj.job_data.title+'</td><td>'+messageShort+'...<br><span class="message-time-date">'+date.getHours()+':'+minutesTwoDigitsWithLeadingZero+' '+date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()+'</span></td></tr>');
 							}else{

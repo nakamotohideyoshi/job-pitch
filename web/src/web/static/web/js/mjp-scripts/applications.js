354f19a92f226d3f4a8411ec0b8af2c72401d94f
unosolo <jchbarreto@gmail.com>
2016-05-26 08:48:59 -0400

    * Refactor script to use the created templates and the common function script.

--- web/src/web/static/web/js/mjp-scripts/applications.js
+++ web/src/web/static/web/js/mjp-scripts/applications.js
@@ -1,9 +1,3 @@
-function goToMessages(id){
-	window.location.href = "/messages/?id="+id;
-}
-function goToUserProfile(id){
-	window.location.href = "/profile/list-jobs/?id="+id;
-}
 
 $(function() {
 	// Run login check funtion with auto-redirect
@@ -16,53 +10,28 @@ $(function() {
 
 	$.get("/api-rest-auth/user/", query)
 	.done(function( user ) {
-		var businessUser = 0;
-		if (user.businesses.length){
-			businessUser = 1; // business
+		var userType = CONST.USER.JOBSEEKER;
+		if (user.businesses.length){ // business
+			userType = CONST.USER.BUSINESS;
 		}
 
 		$.get("/api/application-statuses/", query)
 		.done(function(statuses) {
-			var index = _.findIndex(statuses, ['name', 'CREATED']);
+			var index = _.findIndex(statuses, ['name', CONST.APPLICATION.APPLICATION]);
 			var status = statuses[index];
 
 			var job_id = QueryString.job;
 			if(job_id !== undefined && job_id !== ''){
 				query.job = job_id // Recruiter looking for applications of a job
 			}
+			query.status = status;
 
 			$.get("/api/applications/", query)
 			.done(function( applications ) {
 				var fiteredApplications = _.filter(applications, {'status': status.id});
 
-				if(fiteredApplications.length != 0){
-					for (var key in fiteredApplications) {
-						var obj = fiteredApplications[key];
-						var imageThumb = '';
-						var latest_message = obj.messages[obj.messages.length - 1];
-						var length = 60;
-						var messageShort = latest_message.content.substring(0, length);
-						var date = new Date(latest_message.created);
-						var minutesTwoDigitsWithLeadingZero = ("0" + date.getMinutes()).substr(-2);
-
-						if(businessUser == 1){ //business
-							if(obj.status == 3){ //if deleted
-								$('#list-table tbody')
-								.append('<tr data-application-id="'+obj.id+'" class="application-list inactive" id="application-list-'+obj.id+'"><td>'+obj.job_data.title+'</td><td>'+obj.job_seeker.first_name+' '+obj.job_seeker.last_name+'</td><td>'+obj.job_seeker.description+'</td><td>'+messageShort+'...<br><span class="message-time-date">'+date.getHours()+':'+minutesTwoDigitsWithLeadingZero+' '+date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()+'</span></td><td>Deleted</td></tr>');
-							}else{
-							 	$('#list-table tbody')
-							 	.append('<tr data-application-id="'+obj.id+'" class="application-list" id="application-list-'+obj.id+'"><td>'+obj.job_data.title+'</td><td onclick="viewPitch2(\''+obj.job_seeker.pitches[0].video+'\','+obj.job_data.id+','+obj.job_seeker.id+');">'+obj.job_seeker.first_name+' '+obj.job_seeker.last_name+'</td><td>'+obj.job_seeker.description+'</td><td onclick="goToMessages('+obj.id+');">'+messageShort+'...<br><span class="message-time-date">'+date.getHours()+':'+minutesTwoDigitsWithLeadingZero+' '+date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()+'</span></td><td><a class="delete-action-btn btn btn-primary" href="javascript:deleteRowApplication('+obj.id+' , \'application-list-\');"><i class="fa fa-trash-o"></i></a></td></tr>');
-							}
-						}else{ //job seeker
-							if(obj.status == 3){ //if deleted
-								$('#list-table tbody')
-								.append('<tr data-application-id="'+obj.id+'" class="application-list inactive" id="application-list-'+obj.id+'"><td>'+obj.job_data.title+'</td><td>'+messageShort+'...<br><span class="message-time-date">'+date.getHours()+':'+minutesTwoDigitsWithLeadingZero+' '+date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()+'</span></td></tr>');
-							}else{
-								$('#list-table tbody')
-								.append('<tr data-application-id="'+obj.id+'" class="application-list" id="application-list-'+obj.id+'"><td>'+obj.job_data.title+'</td><td onclick="goToMessages('+obj.id+');">'+messageShort+'...<br><span class="message-time-date">'+date.getHours()+':'+minutesTwoDigitsWithLeadingZero+' '+date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear()+'</span></td></tr>');
-							}
-						}
-					}
+				if( ! _.isEmpty(fiteredApplications)){
+					renderApplications(userType, fiteredApplications, $('#list-table tbody'));
 
 					$('#list-table').show();
 				}else{
@@ -70,7 +39,7 @@ $(function() {
 					$('#no-items-create').show();
 				}
 
-				if (businessUser == 1){
+				if (userType == CONST.USER.BUSINESS){
 					// business
 					$('.business-link').show();
 				}else{

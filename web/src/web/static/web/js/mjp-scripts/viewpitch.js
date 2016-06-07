c156a4e10bf0d34f6b1fc734b6d0a1d3e14b1d84
unosolo <jchbarreto@gmail.com>
2016-06-07 17:03:27 -0400

    Fix: Making changes for adapting profile/viewpitch according to following comment https://github.com/daggaz/my-job-pitch/issues/236#issuecomment-222837205

--- web/src/web/static/web/js/mjp-scripts/viewpitch.js
+++ web/src/web/static/web/js/mjp-scripts/viewpitch.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-	$.get( "/api/pitches/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
+	//$.get( "/api/pitches/", { csrftoken: getCookie('csrftoken') }).done(function( data ) {
 		//console.log(data[0].video);
-		$('.viewYourPitch').html('<video width="320" height="240" controls><source src="'+data[0].video+'" type="video/mp4"></video>');
-	});
+		//$('.viewYourPitch').html('<video width="320" height="240" controls><source src="'+data[0].video+'" type="video/mp4"></video>');
+	//});
+
+	checkIfThereIsApitch();
+
 });

974990967adc2f8c1e32f3f4e343fa58c6222100
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-06-13 18:42:30 +0900

    completed the video recording on iOS

---
+++ ios/My Job Pitch/My Job Pitch/MyAlertController.m
@@ -0,0 +1,46 @@
+//
+//  MyAlertController.m
+//  MyJobPitch
+//
+//  Created by dev on 6/13/16.
+//  Copyright © 2016 SC Labs Ltd. All rights reserved.
+//
+
+#import "MyAlertController.h"
+
+@interface MyAlertController ()
+
+@end
+
+@implementation MyAlertController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+- (BOOL)shouldAutorotate {
+    return false;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

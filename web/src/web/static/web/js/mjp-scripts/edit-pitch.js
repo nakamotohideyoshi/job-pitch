6de7501ee2cc43eacbcf7ebb90c832b60c35abf5
unosolo <jchbarreto@gmail.com>
2016-06-16 10:43:36 -0400

    #236-VideoUploadInterface The recording countdown starts from 30 now.

--- web/src/web/static/web/js/mjp-scripts/edit-pitch.js
+++ web/src/web/static/web/js/mjp-scripts/edit-pitch.js
@@ -1,11 +1,15 @@
 // Getting AWS credentials for uploading file to S3
-var creds =AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+var creds = AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 	IdentityPoolId: 'us-east-1:5e01a88d-f24a-45e8-b1f6-dc9e406fb042'
 });
 AWS.config.credentials = creds;
 AWS.config.region = 'us-east-1';
 
-var bucket = new AWS.S3({params: {Bucket: 'mjp-android-uploads'}});
+var bucket = new AWS.S3({
+	params: {
+		Bucket: 'mjp-android-uploads'
+	}
+});
 
 var recordingTimer;
 
@@ -13,31 +17,31 @@ var actualPitch = null;
 
 var job_seeker_id = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 	checkIfThereIsApitch();
 
-	$('.btn-js-start-pitch').click(function(e) {
+	$('.btn-js-start-pitch').click(function (e) {
 		// prepare video container;
 		$('#pitchVideoCheck video').html('');
 
-		if(onBtnRecordClicked()){
-			startRecordingTimer(31, $('.btn-js-stop-pitch'), stopRecordingProcess);
+		if (onBtnRecordClicked()) {
+			startRecordingTimer(30, $('.btn-js-stop-pitch'), stopRecordingProcess);
 		};
 	});
 
 
-	$('.btn-js-stop-pitch').click(function(e) {
-		stopRecordingProcess( $(this) );
+	$('.btn-js-stop-pitch').click(function (e) {
+		stopRecordingProcess($(this));
 	});
 
 
-	$('.btn-js-upload-pitch').click(function(e) {
+	$('.btn-js-upload-pitch').click(function (e) {
 		$uploadBtn.attr('disabled', true);
-		videoElement.controls= false;
+		videoElement.controls = false;
 
-		if(rawMediaRecorded != undefined && rawMediaRecorded){
-			getNewPitchMetaData(function(pitch){
+		if (rawMediaRecorded != undefined && rawMediaRecorded) {
+			getNewPitchMetaData(function (pitch) {
 				putIntoS3Bucket(pitch, rawMediaRecorded);
 			});
 		}
@@ -46,24 +50,25 @@ $(document).ready(function() {
 
 
 function renderVideoContainer(pitch) {
-	if(pitch !== null && pitch !== undefined && pitch.video !== undefined && pitch.video !== null){
+	if (pitch !== null && pitch !== undefined && pitch.video !== undefined && pitch.video !== null) {
 		var videoSource = '';
 
 		var videoType = 'video/webm';
-		if(pitch.video.indexOf('mp4')>=0){
+		if (pitch.video.indexOf('mp4') >= 0) {
 			videoType = 'video/mp4';
 		}
 
-		videoSource = '<source src="'+pitch.video+'" type="'+videoType+'">';
+		videoSource = '<source src="' + pitch.video + '" type="' + videoType + '">';
 
-		var htmlVideo = '<video width="320" height="240" controls>'+videoSource+'</video><br>';
+		var htmlVideo = '<video width="320" height="240" controls>' + videoSource + '</video><br>';
 		$('#pitchVideoCheck').html(htmlVideo);
 	}
 }
 
 
 function startRecordingTimer(duration, $display, callback) {
-	var timer = duration, minutes, seconds;
+	var timer = duration,
+		minutes, seconds;
 
 	clearInterval(poolingInterval); // Clear any existent pooling process
 
@@ -84,7 +89,7 @@ function startRecordingTimer(duration, $display, callback) {
 }
 
 
-function stopRecordingProcess($display){
+function stopRecordingProcess($display) {
 	$display.text('Stop');
 
 	clearInterval(recordingTimer);
@@ -99,26 +104,25 @@ function getNewPitchMetaData(callback) {
 	$.ajax({
 		url: "/api/pitches/",
 		type: 'POST',
-		data: { csrftoken: getCookie('csrftoken') },
+		data: {
+			csrftoken: getCookie('csrftoken')
+		},
 		cache: false
 	})
-	.done(function(pitch) {
-		callback(pitch);
-	});
+		.done(function (pitch) {
+			callback(pitch);
+		});
 }
 
 
-function putIntoS3Bucket(pitch, object){
+function putIntoS3Bucket(pitch, object) {
 	log('info', 'Start uploading ...');
 
-	object.Key = window.location.origin.replace('//','')
-	+ '/' + pitch.token
-	+ '.' + pitch.id
-	+ '.' + object.Key;
+	object.Key = window.location.origin.replace('//', '') + '/' + pitch.token + '.' + pitch.id + '.' + object.Key;
 
 	bucket.putObject(object, function (err, data) {
-		if(!err){
-			var poolingPromise = new Promise(function(resolve,reject){
+		if (!err) {
+			var poolingPromise = new Promise(function (resolve, reject) {
 				poolingTranscodeProcess(resolve);
 			});
 		}

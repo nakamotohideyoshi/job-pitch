033b92199171970e52b16a5bb729d79e1e6c58ea
unosolo <jchbarreto@gmail.com>
2016-06-23 08:36:06 -0400

    Add an adaptation to the new API, mediaDevices.getUserMedia(), including a polyfill to cope with older browsers. Fix: some new Firefox issuew with the recording process.

--- web/src/web/static/web/js/mediarecorderapi.js
+++ web/src/web/static/web/js/mediarecorderapi.js
@@ -4,8 +4,48 @@
 
 // Spec is at http://dvcs.w3.org/hg/dap/raw-file/tip/media-stream-capture/RecordingProposal.html
 
-navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mediaDevices.getUserMedia || navigator.msGetUserMedia || navigator.mozGetUserMedia || navigator.getUserMedia;
+//navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mediaDevices.getUserMedia || navigator.msGetUserMedia || navigator.mozGetUserMedia || navigator.getUserMedia;
 
+/*
+ * Here an adaptation to the new API, mediaDevices.getUserMedia(),
+ * including a polyfill to cope with older browsers.
+ *
+ * See: https://developer.mozilla.org/es/docs/Web/API/MediaDevices/getUserMedia
+ *
+ */
+
+var promisifiedOldGUM = function (constraints, successCallback, errorCallback) {
+
+	// First get ahold of getUserMedia, if present
+	var getUserMedia = (navigator.getUserMedia ||
+		navigator.webkitGetUserMedia ||
+		navigator.mozGetUserMedia ||
+		navigator.msGetUserMedia);
+
+	// Some browsers just don't implement it - return a rejected promise with an error
+	// to keep a consistent interface
+	if (!getUserMedia) {
+		return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+	}
+
+	// Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+	return new Promise(function (successCallback, errorCallback) {
+		getUserMedia.call(navigator, constraints, successCallback, errorCallback);
+	});
+
+}
+
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+if (navigator.mediaDevices === undefined) {
+	navigator.mediaDevices = {};
+}
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+	navigator.mediaDevices.getUserMedia = promisifiedOldGUM;
+}
 
 if (isBrowser("Chrome")) {
 	var constraints = {
@@ -22,17 +62,17 @@ if (isBrowser("Chrome")) {
 	}; //Chrome
 } else if (isBrowser("Firefox")) {
 	var constraints = {
-		audio: true,
-		video: {
-			width: {
-				min: 320,
-				ideal: 320,
-				max: 1280
+		"audio": true,
+		"video": {
+			"width": {
+				"min": 320,
+				"ideal": 320,
+				"max": 1280
 			},
-			height: {
-				min: 240,
-				ideal: 240,
-				max: 720
+			"height": {
+				"min": 240,
+				"ideal": 240,
+				"max": 720
 			}
 		}
 	}; //Firefox
@@ -189,24 +229,10 @@ function startRecording(stream) {
 		downloadLink.setAttribute("download", name);
 		downloadLink.setAttribute("name", name);
 
-/*		mediaRecorder.stream.getTracks(function (devices) {
-			console.log(devices);
+		if (!isBrowser('Firefox')) {
+			closeMediaDevices();
+		}
 
-			devices.forEach(function (tracks) {
-				console.log(tracks);
-
-				tracks.forEach(function (track) {
-					console.log(track);
-					track.stop();
-				});
-			});
-		});
-*/
-		// Stop active tracks
-		//var tracks = mediaRecorder.stream.getTracks();
-		//tracks.forEach(function (track) {
-		//	track.stop();
-		//});
 	}
 
 	mediaRecorder.onpause = function () {
@@ -250,16 +276,12 @@ function startBeReadyForRecording() {
 			videoElement = videoContainer;
 			videoElement.controls = false;
 
-			if (isBrowser('Firefox') && navigator.mediaDevices) {
-				navigator.mediaDevices.getUserMedia(constraints)
-					.then(function (stream) {
-						beReadyForRecording(stream);
-					});
-			} else {
-				navigator.getUserMedia(constraints, beReadyForRecording, errorCallback);
-			}
+			navigator.mediaDevices.getUserMedia(constraints)
+				.then(function (stream) {
+					beReadyForRecording(stream);
+				});
 
-			return;
+			return stream;
 		});
 }
 
@@ -274,22 +296,17 @@ function onBtnRecordClicked() {
 			videoElement = videoContainer;
 			videoElement.controls = false;
 
-			if (isBrowser('Firefox') && navigator.mediaDevices) {
-				navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+			navigator.mediaDevices.getUserMedia(constraints)
+				.then(function (stream) {
+					recBtn.disabled = true;
+					//	    pauseResBtn.disabled = false;
+					stopBtn.disabled = false;
+					$uploadBtn.prop('disabled', true);
+
 					startRecording(stream);
 				});
-			} else {
-				navigator.getUserMedia(constraints, startRecording, errorCallback);
-			}
 
-			if (successGetUserMedia) {
-				recBtn.disabled = true;
-				//	    pauseResBtn.disabled = false;
-				stopBtn.disabled = false;
-				$uploadBtn.attr('disabled', true);
-			}
-
-			success = successGetUserMedia;
+			return stream;
 		});
 
 	return success;

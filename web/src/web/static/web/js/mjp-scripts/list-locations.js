51a4e47c0eafcbe57d4ec0f40865d5281ef48a37
Julio Herrera <unosolo@users.noreply.github.com>
2016-07-13 15:45:50 -0400

    #264 description in two places (#267)
    * #235-VideoUploadIE
    Adding layout for uploading files.
    
    * #235-VideoUploadIE
    Adding polyfills for using the promise feature in IE.
    
    * #235-VideoUploadIE
    Clean unnecessary comments.
    Adding polyfills for using the promise feature in IE.
    Adding getUserMedia for testing.
    
    * #235-VideoUploadIE
    Clean unnecessary comments.
    Adding polyfills for using the promise feature in IE.
    Adding getUserMedia for testing.
    
    * #235-VideoUploadIE
    Clean unnecessary comments.
    Adding polyfills for using the promise feature in IE.
    Adding getUserMedia for testing.
    
    * #235-VideoUploadIE
    Testing getUserMedia polyfill.
    
    * #235-VideoUploadIE
    Testing getUserMedia polyfill.
    
    * #235-VideoUploadIE
    Uploading video in IE.
    
    * #235-VideoUploadIE
    Uploading video in IE.
    
    * #235-VideoUploadIE
    Uploading video in IE.
    
    * #235-VideoUploadIE
    Spacing for readability.
    fix(IE error): HTML1509: Unmatched end tag. File: edit-pitch, Line: 154, Column: 6
    
    * Merge branch '#235-VideoUpload-IE' of https://github.com/daggaz/my-job-pitch into #235-VideoUpload-IE
    
    * #235-VideoUploadIE
    Set params as the rawmediarecorder properties.
    
    * #235-VideoUploadIE
    Spacing for readability.
    fix(IE error): HTML1509: Unmatched end tag. File: edit-pitch, Line: 154, Column: 6
    These errors could be thrown in IE dued to nest ul tag whithin p tags.
    
    * #235-VideoUploadIE
    Hiding both IE buttons and Html5 button and showing them only when they are in its respective browser.
    
    * #235-VideoUploadIE
    Center file input.
    
    * #264-DescriptionInTwoPlaces
    Refactor and delete some unnecessary code.

--- web/src/web/static/web/js/mjp-scripts/list-locations.js
+++ web/src/web/static/web/js/mjp-scripts/list-locations.js
@@ -1,58 +1,55 @@
-function goToLocations(id){
-	window.location.href = "/profile/list-jobs/?id="+id;
+function goToLocations(id) {
+	window.location.href = "/profile/list-jobs/?id=" + id;
 }
-$(function() {
+$(function () {
 	// Run login check funtion with auto-redirect
 	checkLogin(true);
 
 	//grab business id from url
 	var business_id = QueryString.id;
 
-
-
 	// Add ID to links
-	$('.link-create-location').attr('href', '/profile/create-location/?id='+business_id);
+	$('.link-create-location').attr('href', '/profile/create-location/?id=' + business_id);
 
 	// Populate any fields that have data
-				$.get( "/api/user-locations/", { business:business_id ,csrftoken: getCookie('csrftoken') }).done(function( data ) {
-					$('#companyName').html(data[0].business_data.name);
-					for (var key in data) {
-						var obj = data[key];
-						var imageThumb = '';
+	$.get("/api/user-locations/", {
+		business: business_id,
+		csrftoken: getCookie('csrftoken')
+	}).done(function (data) {
+		$('#companyName').html(data[0].business_data.name);
+		var imageThumb = '';
+		var html = '';
+		var obj;
 
-						console.log(obj);
-						if(obj.images.length != 0){
-							imageThumb = obj.images[0].thumbnail;
-						}
+		for (var key in data) {
+			obj = data[key];
+			imageThumb = '/static/web/images/no_image_available.png';
 
-						if(imageThumb != ''){
-							$('#list-table tbody').append('<tr data-jobs-id="'+obj.id+'" class="locations-list" id="locations-list-'+obj.id+'">'
-								+		'<td onclick="goToLocations('+obj.id+');">'+obj.name+'</td>'
-								+		'<td class="text-center" onclick="goToLocations('+obj.id+');"><img width="150px" src="'+imageThumb+'"></td>'
-								+		'<td onclick="goToLocations('+obj.id+');">'+obj.description+'</td>'
-								+		'<td onclick="goToLocations('+obj.id+');">'+obj.jobs.length+'</td>'
-								+		'<td>'
-								+			'<a class="find-staff-btn btn btn-primary" href="/profile/edit-location/?id='+obj.id+'"><i class="fa fa-pencil"></i></a>'
-								+			' <a class="add-job-btn btn btn-primary" href="/profile/create-job/?companyid='+obj.business+'&id='+obj.id+'"><i class="fa fa-tasks"></i></a>'
-								+			' <a class="delete-action-btn btn btn-primary" href="javascript:deleteRow('+obj.id+',\'user-locations\' , \'jobs-list-\');"><i class="fa fa-trash-o"></i></a>'
-								+		'</td>'
-								+	'</tr>');
-						}else{
-							$('#list-table tbody').append('<tr data-jobs-id="'+obj.id+'" class="locations-list" id="jobs-list-'+obj.id+'">'
-								+		'<td onclick="goToLocations('+obj.id+');">'+obj.description+'</td><td class="text-center" onclick="goToLocations('+obj.id+');"><img width="150px" src="/static/web/images/no_image_available.png">'
-								+		'</td>'
-								+		'<td onclick="goToLocations('+obj.id+');">'+obj.description+'</td><td onclick="goToLocations('+obj.id+');">'+obj.jobs.length+'</td>'
-								+		'<td>'
-								+			'<a class="find-staff-btn btn btn-primary" href="/profile/edit-location/?id='+obj.id+'"><i class="fa fa-pencil"></i></a>'
-								+			' <a class="add-job-btn btn btn-primary" href="/profile/create-job/?companyid='+obj.business+'&id='+obj.id+'"><i class="fa fa-tasks"></i></a>'
-								+			' <a class="delete-action-btn btn btn-primary" href="javascript:deleteRow('+obj.id+',\'user-locations\' , \'jobs-list-\');"><i class="fa fa-trash-o"></i></a>'
-								+		'</td>'
-								+	'</tr>');
-						}
-					}
-				})
-				.fail(function( data ) {
-					console.log( data );
-				});
+			console.log(obj);
+
+			if (obj.images.length != 0) {
+				imageThumb = obj.images[0].thumbnail;
+			}
+
+			html += '<tr data-jobs-id="'+obj.id+'" class="locations-list" id="locations-list-'+obj.id+'">'
+				+		'<td onclick="goToLocations('+obj.id+');">'+obj.name+'</td>'
+				+		'<td class="text-center" onclick="goToLocations('+obj.id+');">'
+				+			'<img width="150px" src="'+imageThumb+'">'
+				+		'</td>'
+				+		'<td onclick="goToLocations('+obj.id+');">'+obj.description+'</td>'
+				+		'<td onclick="goToLocations('+obj.id+');">'+obj.jobs.length+'</td>'
+				+		'<td>'
+				+			'<a class="find-staff-btn btn btn-primary" href="/profile/edit-location/?id='+obj.id+'"><i class="fa fa-pencil"></i></a>'
+				+			' <a class="add-job-btn btn btn-primary" href="/profile/create-job/?companyid='+obj.business+'&id='+obj.id+'"><i class="fa fa-tasks"></i></a>'
+				+			' <a class="delete-action-btn btn btn-primary" href="javascript:deleteRow('+obj.id+',\'user-locations\' , \'jobs-list-\');"><i class="fa fa-trash-o"></i></a>'
+				+		'</td>'
+				+	'</tr>';
+		}
+
+		$('#list-table tbody').append(html);
+
+	}).fail(function (data) {
+		console.log(data);
+	});
 
 });

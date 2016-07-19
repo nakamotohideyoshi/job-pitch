70667629d9a1ea85539744c5f4fc637c4bfbffe9
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-07-19 21:35:40 +0100

    re #242 - Fixed django admin

--- web/src/mjp/admin.py
+++ web/src/mjp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Sex, Nationality, Contract, Hours, Sector, Business, JobSeeker
+from .models import Sex, Nationality, Contract, Hours, Sector, Business, JobSeeker, User
 
 
-@admin.register(Sex, Nationality, Contract, Hours, Sector, Business, JobSeeker)
+@admin.register(Sex, Nationality, Contract, Hours, Sector, Business, JobSeeker, User)
 class Admin(admin.ModelAdmin):
     pass

e42e1dc7559f113ecf4883996bff88a185342e32
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2016-07-21 00:21:26 +0100

    re #87 - Email confirmation

--- web/src/urls.py
+++ web/src/urls.py
@@ -1,17 +1,17 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 from mjp import views as mjp_views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^api/', include(mjp_views.router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-rest-auth/', include('rest_auth.urls')),
     url(r'^api-rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^', include('web.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

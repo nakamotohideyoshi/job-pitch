e28a1595b8e5ac2cb25423599fd5538a487f99e7
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2016-12-07 12:03:34 +0900

    re-updated android purchase

--- app/MyJobPitch/app/src/main/java/com/myjobpitch/api/data/PurchaseInfo.java
+++ app/MyJobPitch/app/src/main/java/com/myjobpitch/api/data/PurchaseInfo.java
@@ -1,26 +1,26 @@
 package com.myjobpitch.api.data;
 
 public class PurchaseInfo {
-    private Integer businessId;
-    private String productId;
-    private String purchaseToken;
+    private Integer business_id;
+    private String product_code;
+    private String purchase_token;
 
     public PurchaseInfo(Integer businessId, String productId, String purchaseToken) {
-        this.businessId = businessId;
-        this.productId = productId;
-        this.purchaseToken = purchaseToken;
+        this.business_id = businessId;
+        this.product_code = productId;
+        this.purchase_token = purchaseToken;
     }
 
-    public Integer getBusinessId() {
-        return businessId;
+    public Integer getBusiness_id() {
+        return business_id;
     }
 
-    public String getProductId() {
-        return productId;
+    public String getProduct_code() {
+        return product_code;
     }
 
-    public String getPurchaseToken() {
-        return purchaseToken;
+    public String getPurchase_token() {
+        return purchase_token;
     }
 
 }

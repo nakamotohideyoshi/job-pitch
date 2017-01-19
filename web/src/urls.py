54ec89c077599abfbf01a091218fc8d8e4343fe2
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-01-19 19:05:26 +0000

    Feature/384 (#391)
    * re #384 - Non-working skeleton API
    
    * re #384 - Added missing library
    
    * re #384 - purchase API work
    
    * re #384 - completed purchase API
    
    * re #384 - Prevented multiple purchases

--- web/src/urls.py
+++ web/src/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from mjp import views as mjp_views
 
 urlpatterns = [
+    url(r'^api/android/purchase/$', mjp_views.AndroidPurchaseView.as_view()),
     url(r'^api/', include(mjp_views.router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-rest-auth/', include('rest_auth.urls')),

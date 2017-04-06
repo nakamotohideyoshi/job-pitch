ef1102c925f752ed94a03ccb6557c9927209fc5a
unosolo <jchbarreto@gmail.com>
2017-04-06 11:32:24 -0400

    fix#502 When logged in, some pages have wrong navigation
    Includes:
       - Default image when recruiter does not have a profile image.
       - fix#444 - Default image for Recruiter Profile
       - Refactor the home pages and related for better performance. For
    this, templates where changed and some javascript were refactored.
       - Refactor how to check if a user is already logged in.
       - Check if a user is getting only its related pages.
       - Adapt business' checkboxes to the new library.

--- web/src/web/static/web/js/mjp-scripts/edit-business.js
+++ web/src/web/static/web/js/mjp-scripts/edit-business.js
@@ -1,133 +1,132 @@
 $(function () {
-	// Run login check funtion with auto-redirect
-	checkLogin(true);
+	app(context).then(function() {
+		//grab business id from url
+		var business_id = QueryString.id;
 
-	//grab business id from url
-	var business_id = QueryString.id;
+		lookUpForCompany(business_id);
 
-	lookUpForCompany(business_id);
+		$('#business').val(business_id);
 
-	$('#business').val(business_id);
+		var query = {
+			token: getCookie('key'),
+			csrftoken: getCookie('csrftoken')
+		};
 
-	var query = {
-		token: getCookie('key'),
-		csrftoken: getCookie('csrftoken')
-	};
+		var userContext = {
+			business_id: business_id,
+		};
 
-	var userContext = {
-		business_id: business_id,
-	};
-
-	userBusinessStore.get(query, userContext).done(function (data) {
-		if (data.name != null) {
-			$('#company_name').val(data.name);
-		}
-
-		$('#currentLogo').attr('src', data.images[0].thumbnail).show();
-	});
-
-	//Form submit code
-	$('#company_details').submit(function (event) {
-		$('.btn-primary').attr("disabled", true);
-		event.preventDefault();
-
-		var company_name = $('#company_name').val();
-		userBusinessStore.put({	name: company_name }, userContext).done(function (data) {
-
-			if ($('#company_logo').val() != '') {
-				var formData = new FormData($('#company_details')[0]);
-
-				userBusinessStore.postImages(formData).done(function(response) {
-					formAlert('success', 'Successfully Updated!').then(function(){
-						$('.btn-primary').attr("disabled", false);
-					});
-
-					userBusinessStore.get(query, userContext).done(function (data) {
-						$('#currentLogo').attr('src', data.images[0].thumbnail).show();
-					});
-				});
-
-			} else {
-				formAlert('success', 'Successfully Updated!', function(){
-					$('.btn-primary').attr("disabled", false);
-				});
+		userBusinessStore.get(query, userContext).done(function (data) {
+			if (data.name !== null) {
+				$('#company_name').val(data.name);
 			}
 
-		})
-			.fail(function (data) {
-				var messageError = ''
-				for (var key in data.responseJSON) {
-					var obj = data.responseJSON[key];
-					messageError = messageError + obj + '<br>';
+			$('#currentLogo').attr('src', data.images[0].thumbnail).show();
+		});
+
+		//Form submit code
+		$('#company_details').submit(function (event) {
+			$('.btn-primary').attr("disabled", true);
+			event.preventDefault();
+
+			var company_name = $('#company_name').val();
+			userBusinessStore.put({	name: company_name }, userContext).done(function (data) {
+
+				if ($('#company_logo').val() !== '') {
+					var formData = new FormData($('#company_details')[0]);
+
+					userBusinessStore.postImages(formData).done(function(response) {
+						formAlert('success', 'Successfully Updated!').then(function(){
+							$('.btn-primary').attr("disabled", false);
+						});
+
+						userBusinessStore.get(query, userContext).done(function (data) {
+							$('#currentLogo').attr('src', data.images[0].thumbnail).show();
+						});
+					});
+
+				} else {
+					formAlert('success', 'Successfully Updated!', function(){
+						$('.btn-primary').attr("disabled", false);
+					});
 				}
-				formAlert('danger', messageError);
-				$('.btn-primary').attr("disabled", false);
-			});
-	});
+			}).fail(function (data) {
+					var messageError = ''
+					for (var key in data.responseJSON) {
+						var obj = data.responseJSON[key];
+						messageError = messageError + obj + '<br>';
+					}
+					formAlert('danger', messageError);
+					$('.btn-primary').attr("disabled", false);
+				});
+		});
 
-	$('#work_place_details').submit(function (event) {
-		event.preventDefault();
+		$('#work_place_details').submit(function (event) {
+			event.preventDefault();
 
-		var work_place_name = $('#work_place_name').val();
-		var work_place_description = $('#work_place_description').val();
-		var work_place_address = $('#work_place_address').val();
-		if ($('#work_place_address_public').is(':checked')) {
-			var work_place_address_public = true;
-		} else {
-			var work_place_address_public = false;
-		}
-		var work_place_email = $('#work_place_email').val();
-		if ($('#work_place_email_public').is(':checked')) {
-			var work_place_email_public = true;
-		} else {
-			var work_place_email_public = false;
-		}
-		var work_place_telephone = $('#work_place_telephone').val();
-		if ($('#work_place_telephone_public').is(':checked')) {
-			var work_place_telephone_public = true;
-		} else {
-			var work_place_telephone_public = false;
-		}
-		var work_place_mobile = $('#work_place_mobile').val();
-		if ($('#work_place_mobile_public').is(':checked')) {
-			var work_place_mobile_public = true;
-		} else {
-			var work_place_mobile_public = false;
-		}
-		var work_place_location = $('#work_place_location').val();
-		var business_id = $('#business').val();
+			var work_place_name = $('#work_place_name').val();
+			var work_place_description = $('#work_place_description').val();
 
-		postcodeLocationData(work_place_location, function (output) {
-			var postcodeData = output.result;
+			var work_place_address = $('#work_place_address').val();
+			var work_place_address_public = $('#work_place_address_public').is(':checked');
 
-			$.post("/api/user-locations/", {
-				name: work_place_name,
-				description: work_place_description,
-				address: work_place_address,
-				email: work_place_email,
-				email_public: work_place_email_public,
-				telephone: work_place_telephone,
-				telephone_public: work_place_telephone_public,
-				mobile: work_place_mobile,
-				mobile_public: work_place_mobile_public,
-				business: business_id,
-				latitude: postcodeData.latitude,
-				longitude: postcodeData.longitude,
-				place_name: postcodeData.nuts
-			}).done(function (data) {
-				$('#location').val(data.id);
-				var formData2 = new FormData($('#work_place_details')[0]);
-				$.ajax({
-					url: '/api/user-location-images/',
-					type: 'POST',
-					data: formData2,
-					cache: false,
-					contentType: false,
-					processData: false,
-					success: function (data) {
-						window.location.href = "/profile/list-businesses/";
-					},
-					fail: function (data) {
+			var work_place_email = $('#work_place_email').val();
+			var work_place_email_public = $('#work_place_email_public').is(':checked');
+
+			var work_place_telephone = $('#work_place_telephone').val();
+			var work_place_telephone_public = $('#work_place_telephone_public').is(':checked');
+
+			var work_place_mobile = $('#work_place_mobile').val();
+			var work_place_mobile_public = $('#work_place_mobile_public').is(':checked');
+
+			var work_place_location = $('#work_place_location').val();
+			var business_id = $('#business').val();
+
+			postcodeLocationData(work_place_location, function (output) {
+				var postcodeData = output.result;
+
+				$.post("/api/user-locations/", {
+					name: work_place_name,
+					description: work_place_description,
+					address: work_place_address,
+					email: work_place_email,
+					email_public: work_place_email_public,
+					telephone: work_place_telephone,
+					telephone_public: work_place_telephone_public,
+					mobile: work_place_mobile,
+					mobile_public: work_place_mobile_public,
+					business: business_id,
+					latitude: postcodeData.latitude,
+					longitude: postcodeData.longitude,
+					place_name: postcodeData.nuts
+				}).done(function (data) {
+					$('#location').val(data.id);
+					var formData2 = new FormData($('#work_place_details')[0]);
+					$.ajax({
+						url: '/api/user-location-images/',
+						type: 'POST',
+						data: formData2,
+						cache: false,
+						contentType: false,
+						processData: false,
+						success: function (data) {
+							window.location.href = "/profile/list-businesses/";
+						},
+						fail: function (data) {
+							var messageError = '';
+
+							for (var key in data.responseJSON) {
+								var obj = data.responseJSON[key];
+								messageError = messageError + obj + '<br>';
+							}
+
+							formAlert('danger', messageError);
+
+							$('.btn-primary').attr("disabled", false);
+						}
+					});
+				})
+					.fail(function (data) {
 						var messageError = '';
 
 						for (var key in data.responseJSON) {
@@ -138,32 +137,19 @@ $(function () {
 						formAlert('danger', messageError);
 
 						$('.btn-primary').attr("disabled", false);
-					}
-				});
-			})
-				.fail(function (data) {
-					var messageError = '';
-
-					for (var key in data.responseJSON) {
-						var obj = data.responseJSON[key];
-						messageError = messageError + obj + '<br>';
-					}
-
-					formAlert('danger', messageError);
-
-					$('.btn-primary').attr("disabled", false);
-				});
+					});
+			});
 		});
-	});
 
-	var text_max = 500;
+		var text_max = 500;
 
-	$('#textarea_feedback').html(text_max + ' characters remaining');
+		$('#textarea_feedback').html(text_max + ' characters remaining');
 
-	$('#work_place_description').keyup(function () {
-		var text_length = $('#work_place_description').val().length;
-		var text_remaining = text_max - text_length;
+		$('#work_place_description').keyup(function () {
+			var text_length = $('#work_place_description').val().length;
+			var text_remaining = text_max - text_length;
 
-		$('#textarea_feedback').html(text_remaining + ' characters remaining');
+			$('#textarea_feedback').html(text_remaining + ' characters remaining');
+		});
 	});
 });
---
+++ web/src/web/static/web/js/mjp-scripts/recruiter-profile-create.js
@@ -0,0 +1,154 @@
+$(function () {
+	app(context).then(function () {
+		var query = context.csrftoken;
+
+		var imageCurrentLogo = '';
+		var formData = '';
+
+		//Form submit code
+		$('#company_details').submit(function (event) {
+			event.preventDefault();
+
+			var company_name = $('#company_name', this).val();
+
+			userBusinessStore.post({
+				name: company_name
+			}).done(function (data) {
+				$('#business').val(data.id);
+
+				var userContext = {
+					business_id: data.id,
+				};
+
+				formData = new FormData($('#company_details')[0]);
+
+				userBusinessStore.postImages(formData).done(function (data) {
+					formAlert('success', 'Successfully Updated!').then(function(){
+						$('.btn-primary').attr("disabled", false);
+					});
+
+					userBusinessStore.get(query, userContext).done(function (data) {
+						$('#currentLogo').attr('src', data.images[0].thumbnail).show();
+					});
+
+					$('#company_details').fadeOut(250, function () {
+						$('#work_place_details').fadeIn(250);
+						$('.page-header').html('Create your first recruitment location');
+					});
+				});
+
+			});
+		});
+
+		$('#work_place_details').submit(function (event) {
+			event.preventDefault();
+
+			var work_place_name = $('#work_place_name').val();
+			var work_place_description = $('#work_place_description').val();
+
+			var work_place_address = $('#work_place_address').val();
+			var work_place_address_public = $('#work_place_address_public').is(':checked');
+
+			var work_place_email = $('#work_place_email').val();
+			var work_place_email_public = $('#work_place_email_public').is(':checked');
+
+			var work_place_telephone = $('#work_place_telephone').val();
+			var work_place_telephone_public = $('#work_place_telephone_public').is(':checked');
+
+			var work_place_mobile = $('#work_place_mobile').val();
+			var work_place_mobile_public = $('#work_place_mobile_public').is(':checked');
+
+			var work_place_location = $('#work_place_location').val();
+			var business_id = $('#business').val();
+
+			postcodeLocationData(work_place_location, function (output) {
+				var postcodeData = output.result;
+
+				$.post("/api/user-locations/", {
+					name: work_place_name,
+					description: work_place_description,
+					address: work_place_address,
+					email: work_place_email,
+					email_public: work_place_email_public,
+					telephone: work_place_telephone,
+					telephone_public: work_place_telephone_public,
+					mobile: work_place_mobile,
+					mobile_public: work_place_mobile_public,
+					business: business_id,
+					latitude: postcodeData.latitude,
+					longitude: postcodeData.longitude,
+					place_name: postcodeData.nuts,
+					postcode_lookup: work_place_location
+				}).done(function (data) {
+					$('#location').val(data.id);
+					var location_id = data.id;
+					if ($('#work_place_image').val() !== '') {
+						var formData2 = new FormData($('#work_place_details')[0]);
+						$.ajax({
+							url: '/api/user-location-images/',
+							type: 'POST',
+							data: formData2,
+							cache: false,
+							contentType: false,
+							processData: false,
+							success: function (data) {
+								console.log(data);
+								window.location.href = "/profile/list-businesses/";
+							},
+							fail: function (data) {
+								var messageError = '';
+								for (var key in data.responseJSON) {
+									var obj = data.responseJSON[key];
+									messageError = messageError + obj + '<br>';
+								}
+								formAlert('danger', messageError);
+								$('.btn-primary').attr("disabled", false);
+							}
+						});
+					} else {
+						formData.append('location', location_id);
+						$.ajax({
+							url: '/api/user-location-images/',
+							type: 'POST',
+							data: formData,
+							cache: false,
+							contentType: false,
+							processData: false,
+							success: function (data) {
+								console.log(data);
+								window.location.href = "/profile/list-businesses/";
+							},
+							fail: function (data) {
+								var messageError = '';
+								for (var key in data.responseJSON) {
+									var obj = data.responseJSON[key];
+									messageError = messageError + obj + '<br>';
+								}
+								formAlert('danger', messageError);
+								$('.btn-primary').attr("disabled", false);
+							}
+						});
+					}
+				})
+					.fail(function (data) {
+						var messageError = '';
+						for (var key in data.responseJSON) {
+							var obj = data.responseJSON[key];
+							messageError = messageError + obj + '<br>';
+						}
+						formAlert('danger', messageError);
+						$('.btn-primary').attr("disabled", false);
+					});
+			});
+		});
+		var text_max = 500;
+		$('#textarea_feedback').html(text_max + ' characters remaining');
+
+		$('#work_place_description').keyup(function () {
+			var text_length = $('#work_place_description').val().length;
+			var text_remaining = text_max - text_length;
+
+			$('#textarea_feedback').html(text_remaining + ' characters remaining');
+		});
+	});
+});

2058d242f3f8874a3fe0187f08b652718998f3bd
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2017-06-21 15:07:05 +0000

    Fixed JS routing issue

--- web/src/urls.py
+++ web/src/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-rest-auth/', include('rest_auth.urls')),
     url(r'^api-rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^', include('web.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('web.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

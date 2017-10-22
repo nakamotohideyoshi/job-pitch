3d4c00b14cb2dc216bb96ebeffbc9ce527874151
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2017-10-22 21:30:13 +0100

    re #734 - Sorting out media files on dev

--- web/src/dev.py
+++ web/src/dev.py
@@ -1,8 +1,6 @@
 from settings import *
 import sys
 
-import os
-
 BASE_DIR = None
 
 globals().update(vars(sys.modules['settings']))
@@ -14,13 +12,13 @@ MIDDLEWARE_CLASSES += (
     'mjp.middleware.PlainExceptionsMiddleware',
 )
 
-EMAIL_PORT = 1025
-
 PAYPAL_CONFIG = {  # paypal-dev@myjobpitch.com, password: Boda100??
     'mode': 'sandbox',
     'client_id': 'AWzWnsMEnHz1_6XpE4rHtYB5rTJ18DH8Lgi6p68V0_XHV5oQrSjxPukTZfu5TPkVneIpFEn0PhJAFN5l',
     'client_secret': 'EJvJP3YjxsZUuvYzV0NEOtS5fUX6C7bGRILVofaHdkjrvfqivWQ8sPXkOZdbLFB67yHuG033640FejsU',
 }
+
+MEDIA_ROOT = '/vagrant/media/'
 
 try:
     from local import *

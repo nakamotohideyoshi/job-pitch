b82cc44e1ae938b8b3d0a2470a33a6421896c7fb
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2017-10-22 20:00:54 +0100

    re #701 - Added limit/offset pagination to API

--- web/src/settings.py
+++ web/src/settings.py
@@ -87,7 +87,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 REST_AUTH_SERIALIZERS = {

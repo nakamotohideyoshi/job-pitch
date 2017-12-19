b7fd3701a6c95579506e695fbc80a4cf90479e04
Jamie Cockburn <jamie_cockburn@hotmail.co.uk>
2017-12-19 16:21:35 +0000

    re #800 - renamed PAYPAL_CONFIG to PAYPAL_TOKEN

--- web/src/staging.py
+++ web/src/staging.py
@@ -11,7 +11,7 @@ USE_X_FORWARDED_HOST = True
 STATIC_ROOT = '/web/mjp/static/'
 MEDIA_ROOT = '/web/mjp/media/'
 
-PAYPAL_CONFIG = {  # paypal-dev@myjobpitch.com, password: Boda100??
+PAYPAL_TOKEN = {  # paypal-dev@myjobpitch.com, password: Boda100??
     'mode': 'sandbox',
     'client_id': 'AWzWnsMEnHz1_6XpE4rHtYB5rTJ18DH8Lgi6p68V0_XHV5oQrSjxPukTZfu5TPkVneIpFEn0PhJAFN5l',
     'client_secret': 'EJvJP3YjxsZUuvYzV0NEOtS5fUX6C7bGRILVofaHdkjrvfqivWQ8sPXkOZdbLFB67yHuG033640FejsU',

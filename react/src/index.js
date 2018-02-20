bebe535e23f8de4f790a21e397087aadce163c52
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-02-21 04:22:53 +0900

    re-build web version

--- react/src/index.js
+++ react/src/index.js
@@ -8,7 +8,6 @@ import App from 'containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './configureStore';
 
-import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/bootstrap.css';
 import 'react-select/dist/react-select.css';
 import 'antd/dist/antd.css';

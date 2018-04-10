0045b146f89a376fb24d37ff9e51affff741569e
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-04-10 17:53:42 -0400

    updated messages page (responsive on all devices)

--- react/src/containers/jobseeker/Messages/styled.js
+++ react/src/containers/jobseeker/Messages/styled.js
@@ -1,37 +1,79 @@
-import styled from 'styled-components';
-import { Layout } from 'antd';
+import styled, { css } from 'styled-components';
 
-export default styled(Layout)`
-  height: calc(100vh - 50px);
+export default styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
 
-  .ant-layout-content {
+  .sidebar {
+    position: absolute;
+    width: 300px;
+    top: 0;
+    bottom: 0;
+    ${({ tablet, open }) => css`
+      left: ${tablet && !open ? '-300px' : '0'};
+    `};
+    z-index: 1;
+    transition: left 0.3s;
+  }
+
+  .body {
+    position: absolute;
+    ${({ tablet }) => css`
+      left: ${tablet ? '0' : '300px'};
+    `};
+    right: 0;
+    top: 0;
+    bottom: 0;
     display: flex;
     flex-direction: column;
 
-    .ant-list-item {
-      padding: 15px !important;
+    .header {
       border-bottom: 1px solid #e0e0e0;
 
-      .ant-avatar {
-        width: 48px;
-        height: 48px;
-        border-radius: 50%;
-      }
-
-      .ant-list-item-meta-title span {
-        color: #ff9300;
+      svg {
+        float: left;
+        width: 19px;
+        height: auto;
+        margin: 28px 20px;
         cursor: pointer;
       }
 
-      .ant-list-item-meta-description span {
-        color: #595959;
-        cursor: pointer;
+      .ant-list-item {
+        padding: 15px !important;
+
+        .ant-avatar {
+          width: 48px;
+          height: 48px;
+          border-radius: 50%;
+        }
+
+        .ant-list-item-meta-title span {
+          color: #ff9300;
+          cursor: pointer;
+        }
+
+        .ant-list-item-meta-description span {
+          color: #595959;
+          cursor: pointer;
+        }
       }
     }
 
     .content {
       flex: 1;
       overflow-y: auto;
+    }
+
+    .mask {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.5);
     }
   }
 `;
--- react/src/containers/recruiter/Messages/index.js
+++ react/src/containers/recruiter/Messages/index.js
@@ -1,28 +1,37 @@
 import React, { Fragment } from 'react';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
-import { Layout, List, Avatar } from 'antd';
+import { List, Avatar } from 'antd';
 
 import { getJobs1 } from 'redux/recruiter/jobs';
 import { getApplications, sendMessage } from 'redux/applications';
 import * as helper from 'utils/helper';
 
-import { AlertMsg, Loading, MessageThread } from 'components';
+import { AlertMsg, Loading, MessageThread, Icons } from 'components';
 import ApplicationDetails from 'containers/recruiter/ApplicationDetails';
 import JobDetails from 'containers/recruiter/JobDetails';
 import Sidebar from './Sidebar';
-import StyledLayout from './styled';
+import Wrapper from './styled';
 
 class Page extends React.Component {
   state = {
     selectedId: null,
     openAppDetails: false,
-    openJobDetails: false
+    openJobDetails: false,
+    tablet: false,
+    open: false
   };
 
   componentWillMount() {
     this.props.getJobs1();
     this.props.getApplications();
+
+    window.addEventListener('resize', this.onResize);
+    this.onResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -33,7 +42,7 @@ class Page extends React.Component {
         const appId = helper.str2int(params.appId) || helper.loadData('messages/appId');
         const app = helper.getItemByID(applications, appId) || applications[0] || {};
         helper.saveData('messages/appId', app.id);
-        this.setState({ selectedId: app.id });
+        this.setState({ selectedId: app.id, open: false });
         this.props.history.replace(`/recruiter/messages/${app.id}`);
       }
     }
@@ -49,10 +58,37 @@ class Page extends React.Component {
     });
   };
 
+  onResize = () => {
+    const tablet = window.innerWidth < 768;
+    const open = !this.state.tablet && tablet ? false : this.state.open;
+    this.setState({ tablet, open });
+  };
+
+  openSidebar = () => this.setState({ open: true });
+  closeSidebar = () => this.setState({ open: false });
   showAppDetails = () => this.setState({ openAppDetails: true });
   hideAppDetails = () => this.setState({ openAppDetails: false });
   showJobDetails = () => this.setState({ openJobDetails: true });
   hideJobDetails = () => this.setState({ openJobDetails: false });
+
+  renderHeader = ({ job_data, job_seeker }) => {
+    const avatar = helper.getPitch(job_seeker).thumbnail;
+    const jobseekerName = helper.getFullJSName(job_seeker);
+    const jobName = helper.getFullBWName(job_data);
+    return (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={avatar} />}
+          title={<span onClick={this.showAppDetails}>{jobseekerName}</span>}
+          description={
+            <span onClick={this.showJobDetails}>
+              {job_data.title} ({jobName})
+            </span>
+          }
+        />
+      </List.Item>
+    );
+  };
 
   render() {
     const { error, jobs, applications } = this.props;
@@ -66,46 +102,36 @@ class Page extends React.Component {
 
     const { selectedId } = this.state;
     const selectedApp = helper.getItemByID(applications, selectedId);
-    const { job_data, job_seeker } = selectedApp;
-    const avatar = helper.getPitch(job_seeker).thumbnail;
-    const jobseekerName = helper.getFullJSName(job_seeker);
-    const jobName = helper.getFullBWName(job_data);
-
-    const { openAppDetails, openJobDetails } = this.state;
+    const { openAppDetails, openJobDetails, tablet, open } = this.state;
 
     return (
-      <StyledLayout>
+      <Wrapper tablet={tablet} open={open}>
         <Sidebar jobs={jobs} applications={applications} selectedId={selectedId} />
 
-        <Layout.Content>
-          {selectedApp && (
-            <Fragment>
-              <List.Item>
-                <List.Item.Meta
-                  avatar={<Avatar src={avatar} />}
-                  title={<span onClick={this.showAppDetails}>{jobseekerName}</span>}
-                  description={
-                    <span onClick={this.showJobDetails}>
-                      {job_data.title} ({jobName})
-                    </span>
-                  }
-                />
-              </List.Item>
-              <div className="content">
+        <div className="body">
+          <Fragment>
+            <div className="header">
+              {tablet && <Icons.Bars onClick={this.openSidebar} />}
+              {selectedApp && this.renderHeader(selectedApp)}
+            </div>
+
+            <div className="content">
+              {selectedApp && (
                 <MessageThread
                   userRole="RECRUITER"
                   application={selectedApp}
                   onSend={this.onSend}
                   onConnect={this.showAppDetails}
                 />
-              </div>
-            </Fragment>
-          )}
-        </Layout.Content>
+              )}
+            </div>
+          </Fragment>
+          {tablet && open && <span className="mask" onClick={this.closeSidebar} />}
+        </div>
 
         {openAppDetails && <ApplicationDetails application={selectedApp} onClose={this.hideAppDetails} />}
-        {openJobDetails && <JobDetails job={job_data} onClose={this.hideJobDetails} />}
-      </StyledLayout>
+        {openJobDetails && <JobDetails job={selectedApp.job_data} onClose={this.hideJobDetails} />}
+      </Wrapper>
     );
   }
 }

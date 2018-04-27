e0662ed6db2c2d57c17c4e047e935bf0a9939972
Kei <plantom46@gmail.com>
2018-04-28 00:12:54 +0400

    re #889 - updated (recruiter/applications, recruiter/jobs, jobseeker/find, jobseeker/applications)

--- react/src/containers/recruiter/Applications/MyConnections/index.js
+++ react/src/containers/recruiter/Applications/MyConnections/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Truncate from 'react-truncate';
-import { List, Modal, Avatar, Tooltip } from 'antd';
+import { List, Modal, Avatar } from 'antd';
 
 import { getApplications, updateApplication, removeApplication } from 'redux/applications';
 import DATA from 'utils/data';
@@ -109,16 +109,12 @@ class MyConnections extends React.Component {
       <List.Item
         key={id}
         actions={[
-          <Tooltip placement="bottom" title="Message">
-            <span onClick={e => this.onMessage(app, e)}>
-              <Icons.Comment />
-            </span>
-          </Tooltip>,
-          <Tooltip placement="bottom" title="Remove">
-            <span onClick={e => this.onRemove(app, e)}>
-              <Icons.TrashAlt />
-            </span>
-          </Tooltip>
+          <span onClick={e => this.onMessage(app, e)}>
+            <Icons.Comment />
+          </span>,
+          <span onClick={e => this.onRemove(app, e)}>
+            <Icons.TrashAlt />
+          </span>
         ]}
         onClick={() => this.onSelect(id)}
         className={loading ? 'loading' : ''}

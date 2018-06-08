ef676428eb3171084b12fecd0a32fdb0317db0cd
Kei <plantom46@gmail.com>
2018-06-08 19:00:33 -0300

    re #946 - updated (web)

--- react/src/containers/Layout/index.js
+++ react/src/containers/Layout/index.js
@@ -6,6 +6,9 @@ import styled from 'styled-components';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import { Link } from 'react-router-dom';
+import colors from 'utils/colors';
+
 const Wrapper = styled(Layout)`
   min-height: 100vh;
 `;
@@ -16,7 +19,27 @@ const Main = styled(Layout)`
   margin-top: 50px;
 `;
 
-const MainLayout = ({ menu, component: Component, ...rest }) => {
+const Banner = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 5px;
+  a {
+    margin-left: 10px;
+    color: white;
+    font-size: 16px;
+  }
+  a:hover {
+    color: white;
+  }
+  > div {
+    padding: 2px 10px 2px 10px;
+    background-color: ${colors.yellow};
+    border-radius: 4px;
+    color: white;
+  }
+`;
+
+const MainLayout = ({ menu, jobseeker, auth, component: Component, ...rest }) => {
   const arr = rest.location.pathname.split('/');
 
   let helpUrl = 'https://www.myjobpitch.com/what-is-my-job-pitch/';
@@ -28,12 +51,27 @@ const MainLayout = ({ menu, component: Component, ...rest }) => {
     helpUrl = 'https://www.myjobpitch.com/jobseeker/';
     url = 'https://www.myjobpitch.com/candidates/';
   }
+  var bannerShown = false;
+  if (auth === 'jobseeker') {
+    bannerShown = !jobseeker.active;
+  }
+
+  var linkShown = rest.location.pathname !== '/jobseeker/settings/profile';
 
   return (
     <Wrapper>
       <Header selectedKey={arr[2]} menu={menu} url={url} />
-
       <Main>
+        {bannerShown ? (
+          <Banner>
+            <div>
+              Your profile is not active!
+              {linkShown ? <Link to="/jobseeker/settings/profile">Activate</Link> : undefined}
+            </div>
+          </Banner>
+        ) : (
+          undefined
+        )}
         <Component {...rest} />
       </Main>
 
@@ -42,4 +80,7 @@ const MainLayout = ({ menu, component: Component, ...rest }) => {
   );
 };
 
-export default MainLayout;
+export default connect(state => ({
+  jobseeker: state.js_profile.jobseeker,
+  auth: state.auth.status
+}))(MainLayout);

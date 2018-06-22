53040db0b0f610b6eb10c15a24d95896ef3c9b94
Kei <plantom46@gmail.com>
2018-06-21 18:56:28 -0700

    re #947 - updated (iOS)

---
+++ ios/My Job Pitch-Swift/MyJobPitch/Views/BusinessUserCell.swift
@@ -0,0 +1,35 @@
+//
+//  BusinessUserCell.swift
+//  MyJobPitch
+//
+//  Created by TIGER1 on 6/21/18.
+//  Copyright © 2018 myjobpitch. All rights reserved.
+//
+
+import UIKit
+import MGSwipeTableCell
+
+class BusinessUserCell: MGSwipeTableCell {
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    
+
+    func setData(_ businessUser: BusinessUser, _ locations: [Location]) {
+        email.text = businessUser.email
+        subTitle.text = nil
+        if businessUser.locations.count == 0 {
+            subTitle.text = "Administrator"
+        } else {
+            for location in locations {
+                if businessUser.locations.contains(location.id) {
+                    if subTitle.text == nil {
+                        subTitle.text = location.name
+                    } else {
+                        subTitle.text = "\(subTitle.text!), \(location.name!)"
+                    }
+                }
+            }
+        }
+    }
+
+}

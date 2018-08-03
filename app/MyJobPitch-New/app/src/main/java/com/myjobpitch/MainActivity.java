c60c55c89a2e4656f94429b8f90030c9e904549b
Kei <plantom46@gmail.com>
2018-08-03 00:29:24 -0700

    re #982 - fixed (iOS, Android)

--- app/MyJobPitch-New/app/src/main/java/com/myjobpitch/MainActivity.java
+++ app/MyJobPitch-New/app/src/main/java/com/myjobpitch/MainActivity.java
@@ -603,15 +603,15 @@ public class MainActivity extends AppCompatActivity
 
     MenuItemInfo[] menuItemData = {
         new MenuItemInfo(AppData.PAGE_FIND_JOB, "Find Job", R.drawable.menu_search, FindJobFragment.class, "P"),
-        new MenuItemInfo(AppData.PAGE_JS_APPLICATIONS, "Applications", R.drawable.menu_application, TalentApplicationsFragment.class, "P"),
+        new MenuItemInfo(AppData.PAGE_JS_APPLICATIONS, "My Applications", R.drawable.menu_application, TalentApplicationsFragment.class, "P"),
         new MenuItemInfo(AppData.PAGE_MESSAGES, "Messages", R.drawable.menu_message, MessageListFragment.class, "PB"),
         new MenuItemInfo(AppData.PAGE_JOB_PROFILE, "Job Profile", R.drawable.menu_job_profile, JobProfileFragment.class, "J"),
         new MenuItemInfo(AppData.PAGE_ADD_RECORD, "Record Pitch", R.drawable.menu_add_record, PitchFragment.class, "J"),
         new MenuItemInfo(AppData.PAGE_VIEW_PROFILE, "Profile", R.drawable.menu_user_profile, TalentDetailFragment.class, ""),
         new MenuItemInfo(AppData.PAGE_USER_PROFILE, "Profile", R.drawable.menu_user_profile, TalentProfileFragment.class, ""),
         new MenuItemInfo(AppData.PAGE_FIND_TALENT, "Find Talent", R.drawable.menu_search, SelectJobFragment.class, "B"),
-        new MenuItemInfo(AppData.PAGE_R_APPLICATIONS, "Applications", R.drawable.menu_application, SelectJobFragment.class, "B"),
-        new MenuItemInfo(AppData.PAGE_CONNECTIONS, "Connections", R.drawable.menu_connect, SelectJobFragment.class, "B"),
+        new MenuItemInfo(AppData.PAGE_R_APPLICATIONS, "New Applications", R.drawable.menu_application, SelectJobFragment.class, "B"),
+        new MenuItemInfo(AppData.PAGE_CONNECTIONS, "My Connections", R.drawable.menu_connect, SelectJobFragment.class, "B"),
         new MenuItemInfo(AppData.PAGE_MY_SHORTLIST, "My Shortlist", R.drawable.menu_shortlisted, SelectJobFragment.class, "B"),
         new MenuItemInfo(AppData.PAGE_ADD_JOB, "Add or Edit Jobs", R.drawable.menu_business, BusinessListFragment.class, ""),
         new MenuItemInfo(AppData.PAGE_PAYMENT, "Payment", R.drawable.menu_payment, PaymentFragment.class, ""),

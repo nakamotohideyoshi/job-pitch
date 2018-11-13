eb77002e7959669359dede245c7ee4a6a470eca5
Jamie Cockburn <jamiec@extramsp.com>
2018-11-13 11:20:47 +0000

    re #MJP-39 - Added apple and android app deeplink assets

--- web/src/web/views.py
+++ web/src/web/views.py
@@ -1,6 +1,23 @@
+from os import path
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+APP_PATH = path.dirname(__file__)
+TEMPLATES_PATH = path.join(APP_PATH, "templates")
 
-# New ones url
+
+def apple_app_site_association(request):
+    filename = path.join(path.join(TEMPLATES_PATH, ".well-known"), "apple-app-site-association")
+    with open(filename) as f:
+        return HttpResponse(f.read(), content_type="application/json")
+
+
+def asset_links(request):
+    filename = path.join(path.join(TEMPLATES_PATH, ".well-known"), "assetlinks.json")
+    with open(filename) as f:
+        return HttpResponse(f.read(), content_type="application/json")
+
+
 def index(request):
     return render(request, 'index.html')

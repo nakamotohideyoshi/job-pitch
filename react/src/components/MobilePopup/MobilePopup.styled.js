09000c11229b1b71ad1703945f5c238f82535561
Alex Reznichenko <alex.reznichenko789@gmail.com>
2018-12-17 12:59:09 +0200

    re #MJP-164 - Android user redirection Play Store  from public link

---
+++ react/src/components/MobilePopup/MobilePopup.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  .google-play-badge > a > img {
+    width: 80%;
+    margin: 0px 10%;
+  }
+  .modal-title-wrapper {
+    text-align: center;
+    padding: 0 15px;
+  }
+  .to-web-btn-wrapper {
+    width: 80%;
+    margin: 10px 10% 0px;
+    height: 63px;
+    text-align: center;
+    & > button {
+      width: 100%;
+      height: 100%;
+      border-radius: 7px;
+    }
+    text-align: center;
+  }
+`;
